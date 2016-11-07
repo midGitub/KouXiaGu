975fa2e167b8ebb4a86c2bf7d34f38691d8c1944
kouxiagu <kouxiagu@gmail.com>
2016-11-07 22:54:54 +0800

    视窗

---
+++ Assets/Scripts/KouXiaGu/Following2D.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using UnityEngine;
+
+namespace KouXiaGu
+{
+
+    /// <summary>
+    /// 根据挂在物体位置跟随目标(非平滑);
+    /// </summary>
+    [DisallowMultipleComponent, ExecuteInEditMode]
+    public class Following2D : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Transform target;
+
+        [SerializeField]
+        private Vector2 spans;
+
+        private Transform current;
+        private Vector3 previousPosition;
+
+        public Transform Target
+        {
+            get { return target; }
+            set { SetTarget(value); }
+        }
+
+        public Vector2 Spans
+        {
+            get { return spans; }
+            set { spans = value; }
+        }
+
+        private void Awake()
+        {
+            current = transform;
+        }
+
+        private void Start()
+        {
+            if (this.target != null)
+            {
+                SetTarget(this.target);
+            }
+        }
+
+        private void Update()
+        {
+            if (target != null && target.position != previousPosition)
+            {
+                CurrentUpdate(previousPosition);
+                previousPosition = target.position;
+            }
+        }
+
+        private void SetTarget(Transform target)
+        {
+            this.target = target;
+            previousPosition = target.position;
+            CurrentUpdate(previousPosition);
+        }
+
+        private void CurrentUpdate(Vector3 targetPosition)
+        {
+            Vector3 currentPosition = this.current.position;
+
+            currentPosition.x = MoveTo(targetPosition.x, currentPosition.x, spans.x);
+            currentPosition.y = MoveTo(targetPosition.y, currentPosition.y, spans.y);
+
+            this.current.position = currentPosition;
+        }
+
+        /// <summary>
+        /// 向目标移动,但是限制在跨度范围内;例如:
+        /// target = 4 , current = 0, spans = 3,返回:3
+        /// target = 5 , current = 3, spans = 3,返回:3
+        /// </summary>
+        /// <param name="target">目标数值</param>
+        /// <param name="current">当前数值</param>
+        /// <param name="spans">从0开始的跨步;</param>
+        /// <returns></returns>
+        private float MoveTo(float target, float current, float spans)
+        {
+            float distanceX = target - current;
+            int multiple = (int)(distanceX / spans);
+
+            if (multiple >= 1 || multiple <= -1)
+            {
+                return current + (spans * multiple);
+            }
+
+            return current;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/KouXiaGu/Test/PositionTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KouXiaGu.Test
+{
+
+    /// <summary>
+    /// 游戏视窗位置测试;
+    /// </summary>
+    [SerializeField]
+    public class PositionTest : MonoBehaviour
+    {
+
+        private Text textObject;
+
+
+        private void Awake()
+        {
+            textObject = GetComponent<Text>();
+        }
+
+        private void Start()
+        {
+            this.ObserveEveryValueChanged(_ => UnityEngine.Input.mousePosition).
+                SubscribeToText(textObject, TextUpdate);
+        }
+
+        private string TextUpdate(Vector3 mousePosition)
+        {
+            string str = "";
+
+            str += GetScreenPoint(mousePosition);
+            str += GetWorldPoint(mousePosition);
+
+            return str;
+        }
+
+        private string GetScreenPoint(Vector3 mousePosition)
+        {
+            string str = "";
+
+            str += "  视窗坐标 :" + mousePosition;
+
+            return str;
+        }
+
+        private string GetWorldPoint(Vector3 mousePosition)
+        {
+            string str = "";
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                str += "  世界坐标 :" + raycastHit.point.ToString();
+            }
+            else
+            {
+                str += "  世界坐标 :" + "无法确定!";
+            }
+            return str;
+        }
+
+
+    }
+
+}
