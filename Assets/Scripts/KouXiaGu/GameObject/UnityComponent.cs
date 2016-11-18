eb6ba78e25ff8f725b2751538a949fe3a68d557d
kouxiagu <kouxiagu@gmail.com>
2016-11-19 05:08:07 +0800

    0.0

--- Assets/Scripts/KouXiaGu/GameObject/UnityComponent.cs
+++ Assets/Scripts/KouXiaGu/GameObject/UnityComponent.cs
@@ -13,18 +13,25 @@ namespace KouXiaGu
     internal static class UnityComponent
     {
 
+        /// <summary>
+        /// 设置参数到;
+        /// </summary>
         public static void Set(this Component instance, Vector3 position, Quaternion rotation)
         {
             Transform transform = instance.transform;
             transform.position = position;
             transform.rotation = rotation;
         }
-
+        /// <summary>
+        /// 设置参数到;
+        /// </summary>
         public static void Set(this Component instance, Transform parent, bool worldPositionStays)
         {
             instance.transform.SetParent(parent, worldPositionStays);
         }
-
+        /// <summary>
+        /// 设置参数到;
+        /// </summary>
         public static void Set(this Component instance, Vector3 position, Quaternion rotation, Transform parent, bool worldPositionStays = false)
         {
             instance.Set(position, rotation);
