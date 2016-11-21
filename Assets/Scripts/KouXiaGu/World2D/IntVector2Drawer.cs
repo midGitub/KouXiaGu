95c19396daf0aba0d6cda1fff15842e22fbbfd39
kouxiagu <kouxiagu@gmail.com>
2016-11-21 21:33:15 +0800

    0.0

---
+++ Assets/Scripts/KouXiaGu/World2D/IntVector2Drawer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace KouXiaGu.World2D
+{
+
+    [CustomPropertyDrawer(typeof(IntVector2))]
+    public class IntVector2Drawer : ShortVector2Drawer
+    {
+
+
+    }
+
+}
---
+++ Assets/Scripts/KouXiaGu/World2D/ShortVector2Drawer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace KouXiaGu.World2D
+{
+
+  
+    [CustomPropertyDrawer(typeof(ShortVector2))]
+    public class ShortVector2Drawer : PropertyDrawer
+    {
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            // Don't make child fields be indented
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            // Calculate rects
+            var Point_x = new Rect(position.x, position.y, position.x / 2- 8, position.height);
+            var Point_y = new Rect(position.x + position.x / 2 - 6, position.y, position.x / 2 - 8, position.height);
+
+            
+            // Draw fields - passs GUIContent.none to each so they are drawn without labels
+            EditorGUI.PropertyField(Point_x, property.FindPropertyRelative("x"), GUIContent.none);
+            EditorGUI.PropertyField(Point_y, property.FindPropertyRelative("y"), GUIContent.none);
+
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+
+        }
+
+    }
+
+}
