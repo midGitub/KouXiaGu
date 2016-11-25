dbcf962850e0cfa0161eb9c6f1c4deb082781972
kouxiagu <kouxiagu@gmail.com>
2016-11-25 18:23:59 +0800

    0.0

---
+++ Assets/Scripts/KouXiaGu/World2D/Navigation/NavAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu.World2D.Navigation
+{
+
+    /// <summary>
+    /// 定义导航时的规则;
+    /// </summary>
+    public class NavAction
+    {
+
+
+    }
+
+}
---
+++ Assets/Scripts/KouXiaGu/World2D/Navigation/Obstruction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KouXiaGu.World2D.Navigation
+{
+
+    /// <summary>
+    /// 寻路障碍物;
+    /// </summary>
+    public class Obstruction : UnitySingleton<Obstruction>, IObstructive<WorldNode, NavAction>
+    {
+
+        public bool CanWalk(NavAction mover, WorldNode item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetCost(NavAction mover, ShortVector2 currentPoint, WorldNode currentNode, ShortVector2 destination)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
