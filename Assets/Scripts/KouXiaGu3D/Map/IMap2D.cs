2c37b85103d7157a615cd80d88533a83a6b2c60b
kouxiagu <kouxiagu@gmail.com>
2016-12-01 12:52:06 +0800

    0.0

---
+++ Assets/Scripts/KouXiaGu3D/Map/IMap2D.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu3D.Map
+{
+
+
+    public interface IMap2D<T> : IEnumerable<KeyValuePair<ShortVector2, T>>
+    {
+
+        T this[ShortVector2 position] { get; set; }
+
+        void Add(ShortVector2 position, T item);
+        bool Remove(ShortVector2 position);
+        bool Contains(ShortVector2 position);
+        bool TryGetValue(ShortVector2 position, out T item);
+
+        void Clear();
+    }
+
+}
