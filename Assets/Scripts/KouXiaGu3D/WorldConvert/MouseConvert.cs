9128f2a3de3c61085aef4ec532ae45f6039ac61d
kouxiagu <kouxiagu@gmail.com>
2016-12-05 23:58:45 +0800

    0

--- Assets/Scripts/KouXiaGu3D/WorldConvert/MouseConvert.cs
+++ Assets/Scripts/KouXiaGu3D/WorldConvert/MouseConvert.cs
@@ -7,14 +7,23 @@ using UnityEngine;
 namespace KouXiaGu
 {
 
-
+    /// <summary>
+    /// 鼠标到地图坐标的转换;
+    /// </summary>
     public static class MouseConvert
     {
 
         /// <summary>
+        /// 碰撞层名称;
+        /// </summary>
+        const string layerName = "SceneAssist";
+
+        /// <summary>
         /// 定义在Unity内触发器所在的层(重要)!
         /// </summary>
-        static readonly int SceneAssistMask = LayerMask.GetMask("SceneAssist");
+        static readonly int layerMask = LayerMask.GetMask(layerName);
+
+        static readonly int layer = LayerMask.NameToLayer(layerName);
 
         /// <summary>
         /// 射线最大距离;
@@ -26,17 +35,41 @@ namespace KouXiaGu
         /// </summary>
         public static Vector3 MouseToPlane(this Camera camera)
         {
+            Start:
+
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit raycastHit;
-            if (Physics.Raycast(ray, out raycastHit, RayMaxDistance, SceneAssistMask, QueryTriggerInteraction.Collide))
+            if (Physics.Raycast(ray, out raycastHit, RayMaxDistance, layerMask, QueryTriggerInteraction.Collide))
             {
                 return raycastHit.point;
             }
             else
             {
+                if (camera.GetComponent<FollowY0>() == null)
+                {
+                    CreateCollider(camera.transform);
+                    goto Start;
+                }
+
                 Debug.LogWarning("坐标无法确定!检查摄像机之前地面是否存在3D碰撞模块!");
                 return default(Vector3);
             }
+        }
+
+        /// <summary>
+        /// 创建一个碰撞器跟随相机;
+        /// </summary>
+        static void CreateCollider(Transform camera)
+        {
+            GameObject collider = new GameObject("MouseCollider");
+            var boxCollider = collider.AddComponent<BoxCollider>();
+
+            boxCollider.isTrigger = true;
+            collider.transform.localScale = new Vector3(50, 0, 50);
+            collider.layer = layer;
+
+            var component = camera.gameObject.AddComponent<FollowY0>();
+            component.Target = collider.transform;
         }
 
         /// <summary>
