5ea46c1c4112b56edd3b18f1995d3c00f78e6a07
kouxiagu <kouxiagu@gmail.com>
2016-12-09 16:07:18 +0800

    0

--- Assets/Scripts/KouXiaGu3D/HexTerrain/Renderer/BasicRenderer.cs
+++ Assets/Scripts/KouXiaGu3D/HexTerrain/Renderer/BasicRenderer.cs
@@ -24,7 +24,7 @@ namespace KouXiaGu.HexTerrain
         /// 在烘焙过程中放置在场景内的物体;
         /// </summary>
         [SerializeField]
-        OvenDisplayMeshQueue ovenDisplayMeshPool;
+        RenderDisplayMeshPool ovenDisplayMeshPool;
 
         [SerializeField, HideInInspector]
         BakingParameter parameter = BakingParameter.Default;
@@ -439,84 +439,83 @@ namespace KouXiaGu.HexTerrain
         //    //RenderTargetManager.ReleaseTexture(rt);
         //}
 
+        ///// <summary>
+        ///// 在烘焙时显示在场景内的网格;
+        ///// </summary>
+        //[Serializable]
+        //class OvenDisplayMeshQueue
+        //{
+        //    OvenDisplayMeshQueue() { }
 
-        /// <summary>
-        /// 在烘焙时显示在场景内的网格;
-        /// </summary>
-        [Serializable]
-        class OvenDisplayMeshQueue
-        {
-            OvenDisplayMeshQueue() { }
+        //    [SerializeField]
+        //    Transform parent;
 
-            [SerializeField]
-            Transform parent;
+        //    [SerializeField]
+        //    MeshRenderer ovenDisplayPrefab;
 
-            [SerializeField]
-            MeshRenderer ovenDisplayPrefab;
+        //    Queue<MeshRenderer> sleep;
+        //    Queue<MeshRenderer> active;
 
-            Queue<MeshRenderer> sleep;
-            Queue<MeshRenderer> active;
+        //    float rotationX;
 
-            float rotationX;
+        //    /// <summary>
+        //    /// 激活在场景的物体;
+        //    /// </summary>
+        //    public IEnumerable<MeshRenderer> Active
+        //    {
+        //        get { return active; }
+        //    }
 
-            /// <summary>
-            /// 激活在场景的物体;
-            /// </summary>
-            public IEnumerable<MeshRenderer> Active
-            {
-                get { return active; }
-            }
+        //    public void Awake()
+        //    {
+        //        sleep = new Queue<MeshRenderer>();
+        //        active = new Queue<MeshRenderer>();
+        //        rotationX = ovenDisplayPrefab.transform.rotation.eulerAngles.x;
+        //    }
 
-            public void Awake()
-            {
-                sleep = new Queue<MeshRenderer>();
-                active = new Queue<MeshRenderer>();
-                rotationX = ovenDisplayPrefab.transform.rotation.eulerAngles.x;
-            }
+        //    /// <summary>
+        //    /// 回收所有激活的物体(将所有激活的物体设为睡眠模式);
+        //    /// </summary>
+        //    public void RecoveryActive()
+        //    {
+        //        while (active.Count != 0)
+        //        {
+        //            var item = active.Dequeue();
+        //            Destroy(item);
+        //        }
+        //    }
 
-            /// <summary>
-            /// 回收所有激活的物体(将所有激活的物体设为睡眠模式);
-            /// </summary>
-            public void RecoveryActive()
-            {
-                while (active.Count != 0)
-                {
-                    var item = active.Dequeue();
-                    Destroy(item);
-                }
-            }
+        //    /// <summary>
+        //    /// 获取到一个网格物体;
+        //    /// </summary>
+        //    public MeshRenderer Dequeue(Vector3 position, float rotationY)
+        //    {
+        //        MeshRenderer mesh;
+        //        Quaternion rotation = Quaternion.Euler(rotationX, rotationY, 0);
+        //        if (sleep.Count == 0)
+        //        {
+        //            mesh = GameObject.Instantiate(ovenDisplayPrefab, position, rotation, parent) as MeshRenderer;
+        //            mesh.gameObject.SetActive(true);
+        //        }
+        //        else
+        //        {
+        //            mesh = sleep.Dequeue();
+        //            mesh.transform.position = position;
+        //            mesh.transform.rotation = rotation;
+        //            mesh.gameObject.SetActive(true);
+        //        }
 
-            /// <summary>
-            /// 获取到一个网格物体;
-            /// </summary>
-            public MeshRenderer Dequeue(Vector3 position, float rotationY)
-            {
-                MeshRenderer mesh;
-                Quaternion rotation = Quaternion.Euler(rotationX, rotationY, 0);
-                if (sleep.Count == 0)
-                {
-                    mesh = GameObject.Instantiate(ovenDisplayPrefab, position, rotation, parent) as MeshRenderer;
-                    mesh.gameObject.SetActive(true);
-                }
-                else
-                {
-                    mesh = sleep.Dequeue();
-                    mesh.transform.position = position;
-                    mesh.transform.rotation = rotation;
-                    mesh.gameObject.SetActive(true);
-                }
+        //        active.Enqueue(mesh);
+        //        return mesh;
+        //    }
 
-                active.Enqueue(mesh);
-                return mesh;
-            }
+        //    void Destroy(MeshRenderer mesh)
+        //    {
+        //        mesh.gameObject.SetActive(false);
+        //        sleep.Enqueue(mesh);
+        //    }
 
-            void Destroy(MeshRenderer mesh)
-            {
-                mesh.gameObject.SetActive(false);
-                sleep.Enqueue(mesh);
-            }
-
-        }
+        //}
 
     }
 
---
+++ Assets/Scripts/KouXiaGu3D/HexTerrain/Renderer/RevisRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KouXiaGu.HexTerrain
+{
+
+
+    /// <summary>
+    /// 对基本地形进行修改的
+    /// </summary>
+    public class RevisRenderer : MonoBehaviour
+    {
+        RevisRenderer() { }
+
+
+
+    }
+
+}
