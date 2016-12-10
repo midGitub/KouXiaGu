56ba5d3bc4ce70dd59f459c977ac35b8b630f464
kouxiagu <kouxiagu@gmail.com>
2016-12-10 23:42:18 +0800

    0

---
+++ Assets/Scripts/KouXiaGu3D/HexTerrain/Map/BlockArchiveExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProtoBuf;
+
+namespace KouXiaGu.HexTerrain
+{
+
+    /// <summary>
+    /// 对 IBlockArchive 接口的拓展;
+    /// </summary>
+    public static class BlockArchiveExtensions
+    {
+
+        /// <summary>
+        /// 保存到这个文件夹下;
+        /// </summary>
+        public static void Save<TP, T>(this BlockArchive<TP, T> block, string directoryPath)
+        {
+            
+        }
+
+    }
+
+}
