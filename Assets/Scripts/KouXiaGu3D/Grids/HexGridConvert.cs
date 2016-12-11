47b8564b035fb56b4b866f1ee134a016bd9e0007
kouxiagu <kouxiagu@gmail.com>
2016-12-11 22:04:21 +0800

    0

--- Assets/Scripts/KouXiaGu3D/Grids/HexGridConvert.cs
+++ Assets/Scripts/KouXiaGu3D/Grids/HexGridConvert.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using KouXiaGu.Grids;
 using UnityEngine;
 
-namespace KouXiaGu.Grids
+namespace KouXiaGu
 {
 
     /// <summary>
