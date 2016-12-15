04c57222773df37489596fcd405557dce5528abe
kouxiagu <kouxiagu@gmail.com>
2016-12-15 15:23:58 +0800

    0

--- Assets/Scripts/KouXiaGu3D/Initialization/Archiver.cs
+++ Assets/Scripts/KouXiaGu3D/Initialization/Archiver.cs
@@ -11,15 +11,6 @@ namespace KouXiaGu.Initialization
 {
 
     /// <summary>
-    /// 可以保存的;
-    /// </summary>
-    public interface IPreservable
-    {
-        IEnumerator OnSave(Archiver archive);
-    }
-
-
-    /// <summary>
     /// 游戏存档管理;
     /// </summary>
     public class Archiver
@@ -61,19 +52,11 @@ namespace KouXiaGu.Initialization
         }
 
         /// <summary>
-        /// 获取到随机的存档文件夹路径,未创建到磁盘;
+        /// 获取到一个随机的文件名;
         /// </summary>
-        static string GetRandomDirectoryPath()
+        static string GetRandomDirectoryName()
         {
-            int appendNumber = 0;
-            string archivedDirectoryPath = Path.Combine(ArchivesPath, Path.GetRandomFileName());
-
-            while (Directory.Exists(archivedDirectoryPath))
-            {
-                archivedDirectoryPath += appendNumber.ToString();
-            }
-
-            return archivedDirectoryPath;
+            return Path.GetRandomFileName();
         }
 
         /// <summary>
@@ -99,7 +82,7 @@ namespace KouXiaGu.Initialization
         /// </summary>
         public Archiver()
         {
-            DirectoryPath = GetRandomDirectoryPath();
+            DirectoryPath = Path.Combine(ArchivesPath, GetRandomDirectoryName());
         }
 
         Archiver(string directoryPath)
@@ -107,117 +90,17 @@ namespace KouXiaGu.Initialization
             this.DirectoryPath = directoryPath;
         }
 
+        /// <summary>
+        /// 当保存完成时调用;
+        /// </summary>
+        public void OnComplete()
+        {
+            Debug.Log("保存完成;路径:" + DirectoryPath);
+            return;
+        }
+
         #endregion
 
     }
-
-
-    public class ArchiveStages : IPeriod
-    {
-        ArchiveStages() { }
-
-        const GameStages DEPUTY = GameStages.Saving;
-        const bool INSTANT = true;
-
-        static readonly ArchiveStages instance = new ArchiveStages();
-        static readonly HashSet<IPreservable> observerSet = new HashSet<IPreservable>();
-        static readonly Queue<IEnumerator> coroutines = new Queue<IEnumerator>();
-
-        static Archiver archive;
-
-
-        GameStages IPeriod.Deputy
-        {
-            get { return DEPUTY; }
-        }
-
-        bool IPeriod.Instant
-        {
-            get { return INSTANT; }
-        }
-
-        bool IPeriod.Premise()
-        {
-            return true;
-            return Initializer.Stages == GameStages.Game;
-        }
-
-        IEnumerator IPeriod.OnEnter()
-        {
-            IEnumerator coroutine;
-            Queue<IEnumerator> coroutineQueue = InitCoroutineQueue();
-
-            while (coroutineQueue.Count != 0)
-            {
-                coroutine = coroutineQueue.Dequeue();
-
-                while (coroutine.MoveNext())
-                {
-                    yield return null;
-                }
-            }
-
-            yield break;
-        }
-
-        Queue<IEnumerator> InitCoroutineQueue()
-        {
-            coroutines.Clear();
-
-            IEnumerator item;
-            foreach (var source in observerSet)
-            {
-                item = source.OnSave(archive);
-                coroutines.Enqueue(item);
-            }
-
-            return coroutines;
-        }
-
-        /// <summary>
-        /// 不需要实现
-        /// </summary>
-        IEnumerator IPeriod.OnLeave()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        /// <summary>
-        /// 订阅到这个状态;
-        /// </summary>
-        public static bool Subscribe(IPreservable observer)
-        {
-            return observerSet.Add(observer);
-        }
-
-        public static bool Unsubscribe(IPreservable observer)
-        {
-            return observerSet.Remove(observer);
-        }
-
-
-        /// <summary>
-        /// 保存游戏为新的存档;
-        /// </summary>
-        public static Archiver Save()
-        {
-            ArchiveStages.archive = new Archiver();
-            Initializer.Add(instance);
-            return archive;
-        }
-
-        /// <summary>
-        /// 保存游戏到存档;
-        /// </summary>
-        public static void Save(Archiver archive)
-        {
-            ArchiveStages.archive = archive;
-            Initializer.Add(instance);
-        }
-
-
-    }
-
 
 }
---
+++ Assets/Scripts/KouXiaGu3D/Initialization/IPreservable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu.Initialization
+{
+
+    /// <summary>
+    /// 可以保存的游戏内容,在存档时调用;
+    /// </summary>
+    public interface IPreservable
+    {
+        IEnumerator OnSave(Archiver archive);
+    }
+
+}
