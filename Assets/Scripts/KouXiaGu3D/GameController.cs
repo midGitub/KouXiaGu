8e20a3846bc9a7655f84cf2cc09b5ebf2f12c022
kouxiagu <kouxiagu@gmail.com>
2016-12-16 15:51:28 +0800

    0

---
+++ Assets/Scripts/KouXiaGu3D/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KouXiaGu
+{
+
+    /// <summary>
+    /// 游戏控制器;
+    /// </summary>
+    [DisallowMultipleComponent]
+    public sealed class GameController : MonoBehaviour
+    {
+
+        public const string Tag = "GameController";
+
+        void Awake()
+        {
+            this.tag = Tag;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public static GameObject GetGameObject()
+        {
+            return GameObject.FindWithTag(Tag);
+        }
+
+    }
+
+}
