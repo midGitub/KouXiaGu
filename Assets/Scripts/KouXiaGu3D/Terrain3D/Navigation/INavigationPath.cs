3d7350d44a16a3a39944abf878b0041986cac3e6
kouxiagu <kouxiagu@gmail.com>
2017-01-01 23:24:45 +0800

    导航

---
+++ Assets/Scripts/KouXiaGu3D/Terrain3D/Navigation/INavigationPath.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KouXiaGu.Terrain3D.Navigation
+{
+
+    /// <summary>
+    /// 路径;
+    /// </summary>
+    public interface INavigationPath
+    {
+
+        /// <summary>
+        /// 移动到的最大速度;
+        /// </summary>
+        float MaxSpeed { get; }
+
+        /// <summary>
+        /// 移动到的位置;
+        /// </summary>
+        Vector3 Position { get; }
+
+        /// <summary>
+        /// 移动到下一个位置,若已经到终点则返回false,否则返回true;
+        /// </summary>
+        bool MoveNext();
+
+        /// <summary>
+        /// 在不使用这路线时调用;
+        /// </summary>
+        void Complete();
+
+    }
+
+}
--- Assets/Scripts/KouXiaGu3D/Terrain3D/Navigation/NavObject.cs
+++ Assets/Scripts/KouXiaGu3D/Terrain3D/Navigation/NavObject.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using UnityEngine;
 
 namespace KouXiaGu.Terrain3D.Navigation
@@ -10,10 +7,139 @@ namespace KouXiaGu.Terrain3D.Navigation
     /// <summary>
     /// 挂载于需要导航的物体;
     /// </summary>
+    [DisallowMultipleComponent]
     public class NavObject : MonoBehaviour
     {
         protected NavObject() { }
 
+        [SerializeField]
+        float maxSpeed = 1;
+
+        [SerializeField]
+        float smoothTime = 0.3f;
+
+        /// <summary>
+        /// 在跟随路径时,当与目标点相距多少距离进行跟换一下个目标点;
+        /// </summary>
+        [SerializeField, Range(0.01f, GridConvert.OuterRadius)]
+        float alternate = 0.3f;
+
+        /// <summary>
+        /// 下一次行动到的目标点;
+        /// </summary>
+        [SerializeField]
+        Vector3 targetPoint;
+
+        /// <summary>
+        /// 当前的速度;
+        /// </summary>
+        Vector3 velocity;
+
+        /// <summary>
+        /// 跟随的线路;
+        /// </summary>
+        INavigationPath path;
+
+        /// <summary>
+        /// 是否正在跟随路线而行动;
+        /// </summary>
+        public bool IsFollowPath
+        {
+            get { return path != null; }
+        }
+
+        /// <summary>
+        /// 当前的速度;
+        /// </summary>
+        public Vector3 Velocity
+        {
+            get { return velocity; }
+        }
+
+        /// <summary>
+        /// 这只导航路径,若已经存在导航路径则替换;
+        /// </summary>
+        public void Follow(INavigationPath path)
+        {
+            if (path == null)
+                throw new ArgumentNullException("空的导航路径;");
+            if(IsFollowPath)
+                CompletePath(this.path);
+
+            this.path = path;
+            MoveNext();
+            enabled = true;
+        }
+
+        void Awake()
+        {
+            if (path == null)
+                enabled = false;
+        }
+
+        void FixedUpdate()
+        {
+            Vector3 current = transform.position;
+            Vector3 next = Vector3.SmoothDamp(current, targetPoint, ref velocity, smoothTime, maxSpeed, Time.deltaTime);
+
+            next.y = GetHeight(next);
+            transform.position = next;
+
+            if (IsClose(current, next))
+            {
+                MoveNext();
+            }
+        }
+
+        /// <summary>
+        /// 这两个点是否足够接近;
+        /// </summary>
+        bool IsClose(Vector3 point1, Vector3 point2)
+        {
+            return (Vector3.Distance(point1, point2) < alternate);
+        }
+
+        /// <summary>
+        /// 设置到下一步;
+        /// </summary>
+        void MoveNext()
+        {
+            if (path.MoveNext())
+            {
+                maxSpeed = path.MaxSpeed;
+                targetPoint = path.Position;
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        /// <summary>
+        /// 停止跟随路径;
+        /// </summary>
+        public void Stop()
+        {
+            CompletePath(path);
+            enabled = false;
+        }
+
+        /// <summary>
+        /// 获取到这个点的高度;
+        /// </summary>
+        float GetHeight(Vector3 point)
+        {
+            return TerrainData.GetHeight(point);
+        }
+
+        /// <summary>
+        /// 完成路线;
+        /// </summary>
+        void CompletePath(INavigationPath path)
+        {
+            path.Complete();
+            path = null;
+        }
 
     }
 
