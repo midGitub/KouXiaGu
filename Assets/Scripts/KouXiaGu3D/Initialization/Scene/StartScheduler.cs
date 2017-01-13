b0168b193e96dfe1e2f226bd8da0fc2803b09735
kouxiagu <kouxiagu@gmail.com>
2017-01-13 15:06:11 +0800

    新的初始化方式

---
+++ Assets/Scripts/KouXiaGu3D/Initialization/Scene/StartScheduler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace KouXiaGu.Initialization
+{
+
+    [DisallowMultipleComponent]
+    public class StartScheduler : MonoBehaviour
+    {
+        StartScheduler() { }
+
+
+        [SerializeField]
+        string gameScene = "Game";
+
+        /// <summary>
+        /// 开始游戏;
+        /// </summary>
+        public void StartGame()
+        {
+            SceneManager.LoadScene(gameScene);
+        }
+
+
+
+
+    }
+
+}
