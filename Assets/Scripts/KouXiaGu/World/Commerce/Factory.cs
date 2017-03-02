7f7f8b9675f37ce4ad74b3f9133b3ad813a0c8b4
Jiong <Jiong@XIAGU>
2017-03-03 01:25:22 +0800

    1

--- Assets/Scripts/KouXiaGu/World/Commerce/Factory.cs
+++ Assets/Scripts/KouXiaGu/World/Commerce/Factory.cs
@@ -7,13 +7,62 @@ namespace KouXiaGu.World.Commerce
 {
 
 
-    public class Factory
+    public class ProductionLine : IEquatable<ProductionLine>
     {
+        public ProductionLine()
+        {
+        }
 
         /// <summary>
-        /// 生产内容;
+        /// 生产资源类型;
         /// </summary>
-        public List<ProductionLine> ProductionLines { get; private set; }
+        public Resource Type { get; set; }
+
+        /// <summary>
+        /// 产品存储到的位置;
+        /// </summary>
+        public ProductRoom ProductRoom { get; set; }
+
+        /// <summary>
+        /// 生产者数目;
+        /// </summary>
+        public byte ProducerCount { get; set; }
+
+
+        /// <summary>
+        /// 生产当前资源,并存储到;
+        /// </summary>
+        public void Producing()
+        {
+
+        }
+
+
+        public bool Equals(ProductionLine other)
+        {
+            return other.Type == this.Type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as ProductionLine;
+
+            if (item == null)
+                return false;
+
+            return Equals(item);
+        }
+
+        public override int GetHashCode()
+        {
+            return Type.GetHashCode();
+        }
+
+    }
+
+
+    public class Factory : List<ProductionLine>
+    {
 
         /// <summary>
         /// 成品储存到;
@@ -26,62 +75,8 @@ namespace KouXiaGu.World.Commerce
         /// </summary>
         /// <param name="type">物品类型;</param>
         /// <param name="productType">生产者数目;</param>
-        public void StartProduction(Resource type, byte producerCount)
+        public void Add(Resource type, byte producerCount)
         {
-
-        }
-
-
-        public class ProductionLine : IEquatable<ProductionLine>
-        {
-            public ProductionLine()
-            {
-            }
-
-            /// <summary>
-            /// 生产资源类型;
-            /// </summary>
-            public Resource Type { get; set; }
-
-            /// <summary>
-            /// 产品存储到的位置;
-            /// </summary>
-            public ProductRoom ProductRoom { get; set; }
-
-            /// <summary>
-            /// 生产者数目;
-            /// </summary>
-            public byte ProducerCount { get; set; }
-
-
-            /// <summary>
-            /// 生产当前资源,并存储到;
-            /// </summary>
-            public void Producing()
-            {
-
-            }
-
-
-            public bool Equals(ProductionLine other)
-            {
-                return other.Type == this.Type;
-            }
-
-            public override bool Equals(object obj)
-            {
-                var item = obj as ProductionLine;
-
-                if (item == null)
-                    return false;
-
-                return Equals(item);
-            }
-
-            public override int GetHashCode()
-            {
-                return Type.GetHashCode();
-            }
 
         }
 
