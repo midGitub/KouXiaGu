93393ce0228787c542dc4e81f06b8838c055009c
Jiong <Jiong@XIAGU>
2017-03-22 01:19:40 +0800

    x

--- Assets/Scripts/KouXiaGu/World/Commerce/Done/Factory.cs
+++ Assets/Scripts/KouXiaGu/World/Commerce/Done/Factory.cs
@@ -5,309 +5,41 @@ using System.Linq;
 namespace KouXiaGu.World.Commerce
 {
 
-    /// <summary>
-    /// 转换内容;
-    /// </summary>
-    public class ProductTransformPremise
+    public class Factory
     {
-
-        public ProductTransformPremise()
-        {
-            Required = new List<ProductBox>();
-            Product = new List<ProductBox>();
-        }
-
-        public ProductTransformPremise(IEnumerable<ProductBox> required, IEnumerable<ProductBox> product)
-        {
-            Required = new List<ProductBox>(required);
-            Product = new List<ProductBox>(product);
-        }
-
-        /// <summary>
-        /// 需要的产品;
-        /// </summary>
-        public List<ProductBox> Required { get; private set; }
-
-        /// <summary>
-        /// 产品的产品;
-        /// </summary>
-        public List<ProductBox> Product { get; private set; }
-
-    }
-
-
-    public class ProductionLine
-    {
-
-        /// <summary>
-        /// 产品的产品;
-        /// </summary>
-        List<ProductItem> product;
-
-
-
-    }
-
-
-    class ProductItem : IDisposable
-    {
-
-        public ProductItem(ProductBox box, ProductWarehouse warehouse)
-        {
-            this.Box = box;
-            this.Wareroom = warehouse.FindOrCreate(box.Product);
-            this.occupyCanceler = Wareroom.Occupy(this);
-        }
-
-        public ProductBox Box { get; private set; }
-        public IWareroom Wareroom { get; private set; }
-        IDisposable occupyCanceler;
-
-        /// <summary>
-        /// 增加产品;
-        /// </summary>
-        public void Add(int multiple)
-        {
-            Wareroom.Add(multiple * Box.Number);
-        }
-
-        /// <summary>
-        /// 移除产品,若无法移除则返回false;
-        /// </summary>
-        public bool Remove(int multiple)
-        {
-            return Wareroom.TryRemove(multiple * Box.Number);
-        }
-
-        public void Dispose()
-        {
-            occupyCanceler.Dispose();
-        }
-
-    }
-
-
-    /// <summary>
-    /// 转换产品到另一种产品;
-    /// </summary>
-    public class ProductTransformation : IDisposable
-    {
-
-        public ProductTransformation(ProductWarehouse warehouse, ProductTransformPremise premise)
-        {
-            MaxProduct = int.MaxValue;
-            this.required = this.CreateList(warehouse, premise.Required);
-            this.product = this.CreateList(warehouse, premise.Product);
-        }
-
-        /// <summary>
-        /// 需要的产品;
-        /// </summary>
-        List<ProductItem> required;
-
-        /// <summary>
-        /// 产品的产品;
-        /// </summary>
-        List<ProductItem> product;
-
-        /// <summary>
-        /// 最大转换次数;
-        /// </summary>
-        public int MaxProduct { get; set; }
-
-        /// <summary>
-        /// 需求;
-        /// </summary>
-        public IEnumerable<ProductBox> Required
-        {
-            get { return required.Select(item => item.Box); }
-        }
 
         /// <summary>
         /// 产出;
         /// </summary>
-        public IEnumerable<ProductBox> Product
+        public IEnumerable<ProductContainer> Output
         {
-            get { return product.Select(item => item.Box); }
-        }
-
-
-        List<ProductItem> CreateList(ProductWarehouse warehouse, IEnumerable<ProductBox> items)
-        {
-            List<ProductItem> list = new List<ProductItem>();
-
-            foreach (var item in items)
-            {
-                list.Add(new ProductItem(item, warehouse));
-            }
-            return list;
+            get { throw new NotImplementedException(); }
         }
 
         /// <summary>
-        /// 进行转换;
+        /// 建造此建筑的前提;
         /// </summary>
-        public void Transform()
+        public bool Precondition(Town town)
         {
-            int number = GetNumberOfProduction(required, MaxProduct);
-
-            foreach (var required in required)
-            {
-                if(!required.Remove(number))
-                {
-                    throw new ArgumentException("无法移除所需的资源");
-                }
-            }
-
-            foreach (var product in product)
-            {
-                product.Add(number);
-            }
-
+            throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// 获取到最大可转换的数目;
-        /// </summary>
-        int GetNumberOfProduction(IEnumerable<ProductItem> products, int maxNumber)
-        {
-            int number = maxNumber;
-
-            foreach (var item in products)
-            {
-                int produce = item.Wareroom.Total / item.Box.Number;
-                if (number > produce)
-                    number = produce;
-            }
-
-            return number;
-        }
-
-
-        public void Dispose()
-        {
-            Dispose(required);
-            Dispose(product);
-        }
-
-        void Dispose(List<ProductItem> items)
-        {
-            foreach (var item in items)
-            {
-                item.Dispose();
-            }
-        }
-
-    }
-
-    /// <summary>
-    /// 产品 和 数量;
-    /// </summary>
-    public class ProductBox
-    {
-        public ProductBox(Product Product, int Number)
-        {
-            this.Product = Product;
-            this.Number = Number;
-        }
-
-        public Product Product { get; private set; }
-        public int Number { get; private set; }
 
     }
 
 
-
-
-
-
-    /// <summary>
-    /// 产出产品;
-    /// </summary>
-    public interface IProduce
+    public interface IFactory
     {
         /// <summary>
-        /// 产品类型;
+        /// 产出;
         /// </summary>
-        Product Product { get; }
-
-        /// <summary>
-        /// 每次更新的产量;
-        /// </summary>
-        int Yields { get; }
+        IEnumerable<ProductContainer> Output { get; }
     }
 
-    public class Factory
+    public class Production
     {
 
-
-
-        public class ProductionLine
-        {
-            public ProductionLine(Product product, IWareroom wareroom)
-            {
-                this.BasicYields = 0;
-                productionInfos = new List<IProduce>();
-                this.Product = product;
-                this.Wareroom = wareroom;
-            }
-
-            /// <summary>
-            /// 基础产量;
-            /// </summary>
-            public int BasicYields { get; private set; }
-            List<IProduce> productionInfos;
-            public Product Product { get; private set; }
-            public IWareroom Wareroom { get; private set; }
-
-            public ProductCategorie Categorie
-            {
-                get { return Product.Categorie; }
-            }
-
-            /// <summary>
-            /// 添加生产项目;
-            /// </summary>
-            public IDisposable Add(IProduce productionInfo)
-            {
-                productionInfos.Add(productionInfo);
-                BasicYields += productionInfo.Yields;
-
-                throw new NotImplementedException();
-            }
-
-            /// <summary>
-            /// 获取到产量;
-            /// </summary>
-            public int GetYields(Months month)
-            {
-                if (IsProductionMonth(month))
-                {
-                    return (int)(
-                        BasicYields * 
-                        Product.ProportionOfProduction * 
-                        Categorie.ProportionOfProduction);
-                }
-                else
-                {
-                    return (int)(
-                        BasicYields *
-                        Product.ProportionOfProduction *
-                        Categorie.ProportionOfProduction *
-                        Product.NonSeasonalPercent);
-                }
-            }
-
-            /// <summary>
-            /// 这个月份是否为合适产出的月份?
-            /// </summary>
-            bool IsProductionMonth(Months month)
-            {
-                int temp = (int)(Product.MonthOfProduction & month);
-                return temp >= 1;
-            }
-
-        }
-
     }
+
 
 }
---
+++ Assets/Scripts/KouXiaGu/World/Commerce/Done/ProductContainer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu.World.Commerce
+{
+
+    /// <summary>
+    /// 表示 产品 和其 数量;
+    /// </summary>
+    public struct ProductContainer : IEquatable<ProductContainer>
+    {
+        public ProductContainer(Product product, int count)
+        {
+            this.Product = product;
+            this.Count = count;
+        }
+
+        public Product Product { get; private set; }
+        public int Count { get; private set; }
+
+        public bool Equals(ProductContainer other)
+        {
+            return
+                this.Product == other.Product &&
+                this.Count == other.Count;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ProductContainer))
+                return false;
+
+            return Equals((ProductContainer)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Product.GetHashCode() ^ Count.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "[Product:" + Product.ToString() + ",Count:" + Count + "]";
+        }
+
+    }
+
+}
