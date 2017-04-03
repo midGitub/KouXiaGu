0c34d0725bb046eaae6df679913282e26de35f0e
Jiong <Jiong@XIAGU>
2017-04-04 02:30:19 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/World/Map/MapFile.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace KouXiaGu.World.Map
+{
+
+
+    public class MapFile
+    {
+        static readonly MapInfoReader infoReader = new MapInfoReader();
+        static readonly MapReader mapReader = new ProtoMapReader();
+
+        public MapInfo Info { get; private set; }
+        public string InfoPath { get; private set; }
+
+        public MapInfoReader InfoReader
+        {
+            get { return infoReader; }
+        }
+
+        public MapReader MapReader
+        {
+            get { return mapReader; }
+        }
+
+        public string InfoFileSearchPattern
+        {
+            get { return "*" + InfoReader.FileExtension; }
+        }
+
+        public MapFile(string filePath)
+        {
+            ReadInfoFile(filePath);
+        }
+
+        public MapFile(string dirPath, SearchOption searchOption)
+        {
+            var filePaths = Directory.GetFiles(dirPath, InfoFileSearchPattern, searchOption);
+
+            if (filePaths.Length == 0)
+                throw new FileNotFoundException();
+
+            string filePath = filePaths[0];
+            ReadInfoFile(filePath);
+        }
+
+        void ReadInfoFile(string filePath)
+        {
+            InfoPath = filePath;
+            Info = InfoReader.Read(filePath);
+        }
+
+        public string GetMapDataFilePath()
+        {
+            return Path.ChangeExtension(InfoPath, MapReader.FileExtension);
+        }
+
+        public Map ReadMap()
+        {
+            string filePath = GetMapDataFilePath();
+            return MapReader.Read(filePath);
+        }
+
+        public void WriteMap(Map data)
+        {
+            string filePath = GetMapDataFilePath();
+            MapReader.Write(filePath, data);
+        }
+
+        public void WriteInfo(MapInfo data)
+        {
+            string filePath = InfoPath;
+            InfoReader.Write(filePath, data);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/KouXiaGu/World/Map/MapInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace KouXiaGu.World.Map
+{
+
+    public class MapInfoReader
+    {
+        public MapInfoReader()
+        {
+            serializer = new XmlSerializer(typeof(MapInfo));
+        }
+
+        XmlSerializer serializer;
+
+        public string FileExtension
+        {
+            get { return ".xml"; }
+        }
+
+        public MapInfo Read(string filePath)
+        {
+            return (MapInfo)serializer.DeserializeXiaGu(filePath);
+        }
+
+        public void Write(string filePath, MapInfo data)
+        {
+            serializer.SerializeXiaGu(filePath, data);
+        }
+    }
+
+    [XmlType("MapInfo")]
+    public struct MapInfo
+    {
+        [XmlAttribute("id")]
+        public int ID { get; set; }
+
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Description")]
+        public string Description { get; set; }
+    }
+
+}
