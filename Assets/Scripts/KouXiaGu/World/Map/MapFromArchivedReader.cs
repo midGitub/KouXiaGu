71693aad67bb6d80e5336fc33c9f7db5041a5c1f
Jiong <Jiong@XIAGU>
2017-04-09 18:57:46 +0800

    x

--- Assets/Scripts/KouXiaGu/World/Map/MapFromArchivedReader.cs
+++ Assets/Scripts/KouXiaGu/World/Map/MapFromArchivedReader.cs
@@ -13,13 +13,15 @@ namespace KouXiaGu.World.Map
     public class MapFromArchivedReader : IMapReader
     {
         const string ArchiveMapInfoFileName = "Map/Info";
+        const string ArchiveMapFileName = "Map/Map";
 
-        public MapFile File { get; private set; }
+        public string ArchiveDir { get; private set; }
+        public MapFile MapFile { get; private set; }
         public ArchiveMapInfo ArchiveInfo { get; private set; }
 
         public MapInfo Info
         {
-            get { return File.Info; }
+            get { return MapFile.Info; }
         }
 
         ArchiveMapInfoReader archiveInfoReader
@@ -27,29 +29,30 @@ namespace KouXiaGu.World.Map
             get { return ArchiveMapInfoReader.DefaultReader; }
         }
 
-        public MapFromArchivedReader(string archiveDir)
+        ArchiveMapReader archiveMapReader
         {
-            ArchiveInfo = ReadArchiveMapInfo(archiveDir);
-            File = FindMapFileOrDefault(ArchiveInfo);
-
-            if (File == null)
-            {
-                throw new FileNotFoundException("未找到对应的地图文件;MapID:" + ArchiveInfo.ID);
-            }
+            get { return ArchiveMapReader.DefaultReader; }
         }
 
-        public Map Read()
+        public MapFromArchivedReader(string archiveDir)
         {
-            return File.ReadMap();
+            ArchiveDir = archiveDir;
+            ArchiveInfo = ReadArchiveMapInfo(archiveDir);
+            MapFile = FindMapFileOrDefault(ArchiveInfo);
+
+            if (MapFile == null)
+                throw new FileNotFoundException("未找到对应的地图文件;MapID:" + ArchiveInfo.ID);
+            if (!File.Exists(GetArchiveMapFilePath(archiveDir)))
+                throw new FileNotFoundException("未找到地图存档;");
         }
 
         ArchiveMapInfo ReadArchiveMapInfo(string archiveDir)
         {
-            string filePath = GetArchiveMapInfoPath(archiveDir);
+            string filePath = GetArchiveMapInfoFilePath(archiveDir);
             return archiveInfoReader.Read(filePath);
         }
 
-        string GetArchiveMapInfoPath(string archiveDir)
+        string GetArchiveMapInfoFilePath(string archiveDir)
         {
             string path = Path.Combine(archiveDir, ArchiveMapInfoFileName);
             Path.ChangeExtension(path, archiveInfoReader.FileExtension);
@@ -61,6 +64,30 @@ namespace KouXiaGu.World.Map
             var maps = MapFileManager.Default.SearchAll();
             var file = maps.FirstOrDefault(item => item.Info.ID == archiveInfo.ID);
             return file;
+        }
+
+        /// <summary>
+        /// 读取到地图,包括存档内容;
+        /// </summary>
+        public Map Read()
+        {
+            Map map = MapFile.ReadMap();
+            ArchiveMap archiveMap = ReadArchiveMap(ArchiveDir);
+            map.Update(archiveMap);
+            return map;
+        }
+
+        ArchiveMap ReadArchiveMap(string archiveDir)
+        {
+            string path = GetArchiveMapFilePath(archiveDir);
+            return archiveMapReader.Read(path);
+        }
+
+        string GetArchiveMapFilePath(string archiveDir)
+        {
+            string path = Path.Combine(archiveDir, ArchiveMapFileName);
+            Path.ChangeExtension(path, archiveMapReader.FileExtension);
+            return path;
         }
 
     }
