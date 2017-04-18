25c5277f6c3f4e864b4c81a17d1795a44aa17d11
Jiong <Jiong@XIAGU>
2017-04-18 20:51:57 +0800

    x

--- Assets/Scripts/KouXiaGu/Rx/UnityThreadDispatcher.cs
+++ Assets/Scripts/KouXiaGu/Rx/UnityThreadDispatcher.cs
@@ -7,68 +7,6 @@ using UnityEngine;
 namespace KouXiaGu.Rx
 {
 
-    public interface IUnityThreadEvent
-    {
-        void OnNext();
-        void OnError(Exception ex);
-    }
-
-    public abstract class UnityDispatcher : IUnityThreadEvent, IDisposable
-    {
-        static UnityThreadDispatcher instance
-        {
-            get { return UnityThreadDispatcher.Instance; }
-        }
-
-        IDisposable disposer;
-
-        public abstract void OnNext();
-        public abstract void OnError(Exception ex);
-
-        public IDisposable SubscribeUpdate()
-        {
-            disposer = instance.SubscribeUpdate(this);
-            return this;
-        }
-
-        public IDisposable SubscribeFixedUpdate()
-        {
-            disposer = instance.SubscribeFixedUpdate(this);
-            return this;
-        }
-
-        public void Dispose()
-        {
-            disposer.Dispose();
-        }
-    }
-
-
-    public class UnityThreadDelegate : UnityDispatcher
-    {
-        public UnityThreadDelegate(Action onNext, Action<Exception> onError)
-        {
-            if (onNext == null)
-                throw new ArgumentNullException("action");
-
-            this.onNext = onNext;
-            this.onError = onError;
-        }
-
-        Action onNext;
-        Action<Exception> onError;
-
-        public override void OnNext()
-        {
-            onNext();
-        }
-
-        public override void OnError(Exception ex)
-        {
-            onError(ex);
-        }
-    }
-
     /// <summary>
     /// 在Unity线程进行的操作;
     /// </summary>
