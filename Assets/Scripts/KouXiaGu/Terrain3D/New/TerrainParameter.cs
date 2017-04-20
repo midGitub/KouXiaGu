7fe56dfc4b6324189f6a053718d6cad3a6745b0d
Jiong <Jiong@XIAGU>
2017-04-20 14:24:47 +0800

    x

--- Assets/Scripts/KouXiaGu/Terrain3D/New/TerrainParameter.cs
+++ Assets/Scripts/KouXiaGu/Terrain3D/New/TerrainParameter.cs
@@ -20,6 +20,69 @@ namespace KouXiaGu.Terrain3D
         /// </summary>
         public Shader TerrainShader;
 
+        /// <summary>
+        /// 地形细分程度;
+        /// </summary>
+        [SerializeField, Range(0, 32)]
+        float tessellation = 16f;
+
+        /// <summary>
+        /// 地形高度缩放;
+        /// </summary>
+        [SerializeField, Range(0, 5)]
+        float displacement = 1.3f;
+
+        /// <summary>
+        /// 降雪程度;
+        /// </summary>
+        [SerializeField, Range(0, 20)]
+        float snowLevel = 0f;
+
+
+        /// <summary>
+        /// 地形细分程度;
+        /// </summary>
+        public float Tessellation
+        {
+            get { return tessellation; }
+        }
+
+        public void SetTessellation(float value)
+        {
+            Shader.SetGlobalFloat("_TerrainTess", value);
+            tessellation = value;
+        }
+
+
+        /// <summary>
+        /// 地形高度缩放;
+        /// </summary>
+        public float Displacement
+        {
+            get { return displacement; }
+        }
+
+        public void SetDisplacement(float value)
+        {
+            Shader.SetGlobalFloat("_TerrainDisplacement", value);
+            displacement = value;
+        }
+
+
+        /// <summary>
+        /// 降雪程度;
+        /// </summary>
+        public float SnowLevel
+        {
+            get { return snowLevel; }
+        }
+
+        public void SetSnowLevel(float value)
+        {
+            Shader.SetGlobalFloat("_TerrainSnow", value);
+            snowLevel = value;
+        }
+
     }
 
 }
