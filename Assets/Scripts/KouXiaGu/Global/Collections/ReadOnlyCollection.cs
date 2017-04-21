4742f9a77e83503a9ada08e15c2ba5b38d97b8cf
Jiong <Jiong@XIAGU>
2017-04-21 16:15:03 +0800

    x

--- Assets/Scripts/KouXiaGu/Global/Collections/ReadOnlyCollection.cs
+++ Assets/Scripts/KouXiaGu/Global/Collections/ReadOnlyCollection.cs
@@ -23,71 +23,9 @@ namespace KouXiaGu
         bool TryGetValue(TKey key, out TValue value);
     }
 
-
-    class _ReadOnlyCollection<T> : IReadOnlyCollection<T>
-    {
-        public _ReadOnlyCollection(ICollection<T> collection)
-        {
-            this.collection = collection;
-        }
-
-        ICollection<T> collection;
-
-        public int Count
-        {
-            get { return collection.Count; }
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            return collection.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return collection.GetEnumerator();
-        }
-    }
-
-    class _ReadOnlyDictionary<TKey, TValue> : _ReadOnlyCollection<KeyValuePair<TKey, TValue>>, IReadOnlyDictionary<TKey, TValue>
-    {
-        public _ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary)
-            : base(dictionary)
-        {
-            this.dictionary = dictionary;
-        }
-
-        IDictionary<TKey, TValue> dictionary;
-
-        public IEnumerable<TKey> Keys
-        {
-            get { return dictionary.Keys; }
-        }
-
-        public IEnumerable<TValue> Values
-        {
-            get { return dictionary.Values; }
-        }
-
-        public TValue this[TKey key]
-        {
-            get { return dictionary[key]; }
-        }
-
-        public bool ContainsKey(TKey key)
-        {
-            return dictionary.ContainsKey(key);
-        }
-
-        public bool TryGetValue(TKey key, out TValue value)
-        {
-            return dictionary.TryGetValue(key, out value);
-        }
-
-    }
-
     public static class ReadOnlyExtensions
     {
+
         /// <summary>
         /// 转转换到只读接口;
         /// </summary>
@@ -96,6 +34,32 @@ namespace KouXiaGu
             return new _ReadOnlyCollection<T>(collection);
         }
 
+        class _ReadOnlyCollection<T> : IReadOnlyCollection<T>
+        {
+            public _ReadOnlyCollection(ICollection<T> collection)
+            {
+                this.collection = collection;
+            }
+
+            ICollection<T> collection;
+
+            public int Count
+            {
+                get { return collection.Count; }
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                return collection.GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return collection.GetEnumerator();
+            }
+        }
+
+
         /// <summary>
         /// 转换到只读接口;
         /// </summary>
@@ -103,6 +67,44 @@ namespace KouXiaGu
         {
             return new _ReadOnlyDictionary<TKey, TValue>(dictionary);
         }
+
+        class _ReadOnlyDictionary<TKey, TValue> : _ReadOnlyCollection<KeyValuePair<TKey, TValue>>, IReadOnlyDictionary<TKey, TValue>
+        {
+            public _ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary)
+                : base(dictionary)
+            {
+                this.dictionary = dictionary;
+            }
+
+            IDictionary<TKey, TValue> dictionary;
+
+            public IEnumerable<TKey> Keys
+            {
+                get { return dictionary.Keys; }
+            }
+
+            public IEnumerable<TValue> Values
+            {
+                get { return dictionary.Values; }
+            }
+
+            public TValue this[TKey key]
+            {
+                get { return dictionary[key]; }
+            }
+
+            public bool ContainsKey(TKey key)
+            {
+                return dictionary.ContainsKey(key);
+            }
+
+            public bool TryGetValue(TKey key, out TValue value)
+            {
+                return dictionary.TryGetValue(key, out value);
+            }
+
+        }
+
     }
 
 }
