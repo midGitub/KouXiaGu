1be0e2147b08b7d0cbb0bc46caa0a4921852bb80
Jiong <Jiong@XIAGU>
2017-04-21 17:46:36 +0800

    x

--- Assets/Scripts/KouXiaGu/Terrain3D/New/ChunkBuilder.cs
+++ Assets/Scripts/KouXiaGu/Terrain3D/New/ChunkBuilder.cs
@@ -8,13 +8,13 @@ using KouXiaGu.Grids;
 namespace KouXiaGu.Terrain3D
 {
 
+
     class ChunkBuilder : AsyncOperation<Chunk>
     {
         public static ChunkBuilder Create(RectCoord chunkCoord, ChunkManager chunk)
         {
             return new ChunkBuilder(chunkCoord);
         }
-
 
         ChunkBuilder(RectCoord chunkCoord)
         {
@@ -47,50 +47,5 @@ namespace KouXiaGu.Terrain3D
             return ChunkCoord.GetHashCode();
         }
     }
-
-
-    //public class ChunkBuildManager
-    //{
-    //    public ChunkBuildManager()
-    //    {
-    //        buildingChunks = new Dictionary<RectCoord, ChunkBuilder>();
-    //        TerrainChunk = new ChunkManager();
-    //    }
-
-    //    Dictionary<RectCoord, ChunkBuilder> buildingChunks;
-    //    public ChunkManager TerrainChunk { get; private set; }
-
-    //    public IAsyncOperation<Chunk> Create(RectCoord chunkCoord)
-    //    {
-    //        ChunkBuilder builder;
-
-    //        if (!buildingChunks.TryGetValue(chunkCoord, out builder))
-    //        {
-    //            //builder = ChunkBuilder.Create(chunkCoord);
-    //            buildingChunks.Add(chunkCoord, builder);
-    //            throw new NotImplementedException();
-    //        }
-
-    //        return builder;
-    //    }
-
-    //    class MapObserver : IDictionaryObserver<CubicHexCoord, MapNode>
-    //    {
-    //        void IDictionaryObserver<CubicHexCoord, MapNode>.OnAdded(CubicHexCoord key, MapNode newValue)
-    //        {
-    //            throw new NotImplementedException();
-    //        }
-
-    //        void IDictionaryObserver<CubicHexCoord, MapNode>.OnRemoved(CubicHexCoord key, MapNode originalValue)
-    //        {
-    //            throw new NotImplementedException();
-    //        }
-
-    //        void IDictionaryObserver<CubicHexCoord, MapNode>.OnUpdated(CubicHexCoord key, MapNode originalValue, MapNode newValue)
-    //        {
-    //            throw new NotImplementedException();
-    //        }
-    //    }
-    //}
 
 }
