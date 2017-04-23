703604de19600752d101878bf7ddf908fff23be1
Jiong <Jiong@XIAGU>
2017-04-23 20:58:19 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/World/Initializer/WorldDataInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu.World
+{
+
+    /// <summary>
+    /// 游戏场景数据初始化;
+    /// </summary>
+    public class WorldDataInitializer
+    {
+
+    }
+
+}
