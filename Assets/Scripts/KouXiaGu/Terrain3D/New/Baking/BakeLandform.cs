ab1343521abdc188e164ac933d4fe15978ff8288
Jiong <Jiong@XIAGU>
2017-04-24 21:47:05 +0800

    x

--- Assets/Scripts/KouXiaGu/Terrain3D/New/Baking/BakeLandform.cs
+++ Assets/Scripts/KouXiaGu/Terrain3D/New/Baking/BakeLandform.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using KouXiaGu.Grids;
 using KouXiaGu.World;
 using KouXiaGu.World.Map;
@@ -21,43 +19,42 @@ namespace KouXiaGu.Terrain3D
         {
         }
 
-        public MeshRenderer _prefab;
-        public int _maxCapacity = 100;
+        public MeshRenderer prefab;
+        public int maxCapacity = 100;
         GameObjectPool<MeshRenderer> objectPool;
 
-        public Shader _diffuseShader;
-        public Shader _heightShader;
+        public Shader diffuseShader;
+        public Shader heightShader;
         Material diffuseMaterial;
         Material heightMaterial;
 
         List<Pack> sceneObjects;
 
         public IWorldData WorldData { get; private set; }
-        public RectCoord ChunkCoord { get; private set; }
+        public CubicHexCoord ChunkCenter { get; private set; }
         public RenderTexture DiffuseRT { get; private set; }
         public RenderTexture HeightRT { get; private set; }
 
         public void Initialise()
         {
-            objectPool = new GameObjectPool<MeshRenderer>(_prefab, _maxCapacity);
-            diffuseMaterial = new Material(_diffuseShader);
-            heightMaterial = new Material(_heightShader);
+            objectPool = new GameObjectPool<MeshRenderer>(prefab, maxCapacity);
+            diffuseMaterial = new Material(diffuseShader);
+            heightMaterial = new Material(heightShader);
             sceneObjects = new List<Pack>();
         }
 
         /// <summary>
         /// 开始烘培任务;
         /// </summary>
-        public IEnumerator Bake(IWorldData worldData, RectCoord chunkCoord, IEnumerable<CubicHexCoord> displays)
+        public IEnumerator Bake(IWorldData worldData, CubicHexCoord chunkCenter, IEnumerable<CubicHexCoord> displays)
         {
             WorldData = worldData;
-            ChunkCoord = chunkCoord;
+            ChunkCenter = chunkCenter;
 
             PrepareScene(displays);
-            yield return null;
-
-
+            BakeDiffuse();
             ClearScene();
+            yield break;
         }
 
         /// <summary>
@@ -67,7 +64,7 @@ namespace KouXiaGu.Terrain3D
         {
             foreach (var roadMesh in sceneObjects)
             {
-                Release(roadMesh.Rednerer);
+                GameObject.Destroy(roadMesh.Rednerer);
             }
             sceneObjects.Clear();
         }
@@ -84,7 +81,7 @@ namespace KouXiaGu.Terrain3D
 
                 if (info != null)
                 {
-                    var mesh = Get(display, angle, -sceneObjects.Count);
+                    var mesh = CreateMeshRenderer(display, angle, -sceneObjects.Count);
                     sceneObjects.Add(new Pack(info, mesh));
                 }
             }
@@ -134,7 +131,7 @@ namespace KouXiaGu.Terrain3D
             return info;
         }
 
-        public MeshRenderer Get(CubicHexCoord coord, float angle, float y)
+        public MeshRenderer CreateMeshRenderer(CubicHexCoord coord, float angle, float y)
         {
             MeshRenderer item = objectPool.Get();
             item.transform.position = coord.GetTerrainPixel(y);
@@ -142,10 +139,63 @@ namespace KouXiaGu.Terrain3D
             return item;
         }
 
-        public void Release(MeshRenderer mesh)
+
+        /// <summary>
+        /// 完全透明颜色;
+        /// </summary>
+        static readonly Color Transparent = new Color(0, 0, 0, 0);
+
+        void BakeDiffuse()
         {
-            objectPool.Release(mesh);
+            foreach (var meshRenderer in sceneObjects)
+            {
+                SetDiffuserMaterial(meshRenderer);
+            }
+
+            DiffuseRT = BakeCamera.GetDiffuseTemporaryRender();
+            BakeCamera.CameraRender(DiffuseRT, ChunkCenter, Transparent);
         }
+
+        void SetDiffuserMaterial(Pack renderer)
+        {
+            TerrainLandform res = renderer.Res;
+
+            if (renderer.Rednerer.material != null)
+            {
+                GameObject.Destroy(renderer.Rednerer.material);
+            }
+
+            var material = renderer.Rednerer.material = new Material(diffuseMaterial);
+            material.SetTexture("_MainTex", res.DiffuseTex);
+            material.SetTexture("_BlendTex", res.DiffuseBlendTex);
+        }
+
+        void BakeHeight()
+        {
+            foreach (var meshRenderer in sceneObjects)
+            {
+                SetHeightMaterial(meshRenderer);
+            }
+
+            HeightRT = BakeCamera.GetHeightTemporaryRender();
+            BakeCamera.CameraRender(HeightRT, ChunkCenter);
+        }
+
+        void SetHeightMaterial(Pack renderer)
+        {
+            TerrainLandform res = renderer.Res;
+
+            if (renderer.Rednerer.material != null)
+            {
+                GameObject.Destroy(renderer.Rednerer.material);
+                renderer.Rednerer.material = heightMaterial;
+            }
+
+            var material = renderer.Rednerer.material;
+            material.SetTexture("_MainTex", res.HeightTex);
+            material.SetTexture("_BlendTex", res.HeightBlendTex);
+        }
+
 
         /// <summary>
         /// 释放所有该实例创建的 RenderTexture 类型的资源;
