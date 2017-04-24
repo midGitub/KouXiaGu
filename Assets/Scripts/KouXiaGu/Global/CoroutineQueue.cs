8ffff7db7ffd8802c1572f08fa3716037732a207
Jiong <Jiong@XIAGU>
2017-04-24 17:49:56 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/Global/CoroutineQueue.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu
+{
+
+    /// <summary>
+    /// 协程队列;
+    /// </summary>
+    public class CoroutineQueue<T> : IEnumerator
+        where T : IEnumerator
+    {
+        public CoroutineQueue(ISegmented stopwatch)
+        {
+            requestQueue = new Queue<T>();
+            coroutineStack = new Stack<IEnumerator>();
+            Stopwatch = stopwatch;
+        }
+
+        readonly Queue<T> requestQueue;
+        readonly Stack<IEnumerator> coroutineStack;
+        public ISegmented Stopwatch { get; set; }
+
+        object IEnumerator.Current
+        {
+            get { return null; }
+        }
+
+        public void Next()
+        {
+            if (coroutineStack.Count != 0)
+            {
+                Stopwatch.Restart();
+                while (!Stopwatch.Await())
+                {
+                    IEnumerator request = coroutineStack.Peek();
+
+                    bool moveNext = request.MoveNext();
+                    if (!moveNext)
+                    {
+                        coroutineStack.Pop();
+                        if (coroutineStack.Count == 0)
+                        {
+                            requestQueue.Dequeue();
+                            if (requestQueue.Count == 0)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                T item = requestQueue.Peek();
+                                coroutineStack.Push(item);
+                            }
+                        }
+                        continue;
+                    }
+
+                    var newCoroutine = request.Current as IEnumerator;
+                    if (newCoroutine != null)
+                    {
+                        coroutineStack.Push(newCoroutine);
+                    }
+                }
+            }
+        }
+
+        public void Add(T item)
+        {
+            requestQueue.Enqueue(item);
+            if (coroutineStack.Count == 0)
+            {
+                coroutineStack.Push(item);
+            }
+        }
+
+        public void Clear()
+        {
+            requestQueue.Clear();
+            coroutineStack.Clear();
+        }
+
+        bool IEnumerator.MoveNext()
+        {
+            Next();
+            return true;
+        }
+
+        void IEnumerator.Reset()
+        {
+            return;
+        }
+
+    }
+
+}
