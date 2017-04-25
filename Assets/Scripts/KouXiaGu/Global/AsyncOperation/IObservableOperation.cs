c980c0878f62919d15bea080e883bcda795d165c
Jiong <Jiong@XIAGU>
2017-04-26 02:10:58 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/Global/AsyncOperation/IObservableOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu
+{
+
+    interface IObservableOperation
+    {
+        IDisposable Subscribe(IOperationObserver observer);
+    }
+
+    interface IObservableOperation<T>
+    {
+        IDisposable Subscribe(IOperationObserver<T> observer);
+    }
+
+    interface IOperationObserver
+    {
+        void OnCompleted();
+        void OnFaulted(Exception ex);
+        void OnCanceled();
+    }
+
+    interface IOperationObserver<T>
+    {
+        void OnCompleted(T item);
+        void OnFaulted(Exception ex);
+        void OnCanceled();
+    }
+
+}
