490f7d80b6baeade5bebd396d42cebe02f8b1efa
Jiong <Jiong@XIAGU>
2017-04-25 17:52:40 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/Global/Rx/Unsubscriber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu
+{
+
+    public class Unsubscriber : IDisposable
+    {
+        public Unsubscriber(Action disposer)
+        {
+            if (disposer == null)
+                throw new ArgumentNullException();
+
+            isUnsubscribe = false;
+            this.disposer = disposer;
+        }
+
+        bool isUnsubscribe;
+        Action disposer;
+
+        public void Dispose()
+        {
+            if (!isUnsubscribe)
+            {
+                isUnsubscribe = true;
+                disposer();
+            }
+        }
+    }
+
+
+}
