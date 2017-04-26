db732c9c8caf34e0fbe5ec04ae6da066a048a247
Jiong <Jiong@XIAGU>
2017-04-26 19:18:21 +0800

    x

--- Assets/Scripts/KouXiaGu/Terrain3D/Landform.cs
+++ Assets/Scripts/KouXiaGu/Terrain3D/Landform.cs
@@ -18,18 +18,11 @@ namespace KouXiaGu.Terrain3D
         public static Landform Initialize(IWorldData worldData)
         {
             var item = SceneObject.GetObject<Landform>();
-            item.worldData = worldData;
+            item.Builder = new LandformBuilder(worldData);
             return item;
         }
 
-        IWorldData worldData;
-        public ChunkSceneManager ChunkManager { get; private set; }
         public LandformBuilder Builder { get; private set; }
-
-        void Awake()
-        {
-            ChunkManager = new ChunkSceneManager();
-        }
 
         /// <summary>
         /// 获取到高度,若不存在高度信息,则返回0;
@@ -37,11 +30,11 @@ namespace KouXiaGu.Terrain3D
         public float GetHeight(Vector3 position)
         {
             RectCoord chunkCoord = ChunkInfo.ChunkGrid.GetCoord(position);
-            Chunk chunk;
-            if (ChunkManager.InSceneChunks.TryGetValue(chunkCoord, out chunk))
+            IAsyncOperation<Chunk> chunk;
+            if (Builder.SceneChunks.TryGetValue(chunkCoord, out chunk))
             {
                 Vector2 uv = ChunkInfo.ChunkGrid.GetUV(chunkCoord, position);
-                return chunk.Renderer.GetHeight(uv);
+                return chunk.Result.Renderer.GetHeight(uv);
             }
             return 0;
         }
