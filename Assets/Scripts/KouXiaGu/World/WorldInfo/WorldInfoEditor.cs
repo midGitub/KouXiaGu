9f5dea3e9613921e59d301bd12343c9f789577df
Jiong <Jiong@XIAGU>
2017-04-26 17:07:44 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/World/WorldInfo/WorldInfoEditor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KouXiaGu.World
+{
+
+
+    [DisallowMultipleComponent, RequireComponent(typeof(WorldInitializer))]
+    public class WorldInfoEditor : MonoBehaviour
+    {
+
+        public bool UseRandomMap;
+
+        class WorldInfoReader : AsyncOperation<WorldInfo>
+        {
+            public WorldInfoReader(WorldInfoEditor info)
+            {
+                this.info = info;
+                worldInfo = new WorldInfo();
+            }
+
+            WorldInfoEditor info;
+            WorldInfo worldInfo;
+
+            void Start()
+            {
+
+            }
+
+        }
+    }
+
+}
