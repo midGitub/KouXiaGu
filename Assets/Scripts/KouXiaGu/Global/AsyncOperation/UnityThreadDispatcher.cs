adc2b1bc018145c18fa0bd30ab34c08e3bfaecfd
Jiong <Jiong@XIAGU>
2017-04-28 20:17:18 +0800

    x

--- Assets/Scripts/KouXiaGu/Global/AsyncOperation/UnityThreadDispatcher.cs
+++ Assets/Scripts/KouXiaGu/Global/AsyncOperation/UnityThreadDispatcher.cs
@@ -29,7 +29,7 @@ namespace KouXiaGu
 
         public override string ToString()
         {
-            return "[Sender:" + Sender.ToString() + "]";
+            return Sender.ToString();
         }
     }
 
