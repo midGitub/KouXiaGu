44240e3cf319cc69012fe4de38660b17e667c261
Jiong <Jiong@XIAGU>
2017-04-28 17:57:05 +0800

    x

--- Assets/Scripts/KouXiaGu/Global/UnityThread/UnityThreadDispatcher.cs
+++ Assets/Scripts/KouXiaGu/Global/UnityThread/UnityThreadDispatcher.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
-
 using UniRx;
 
 namespace KouXiaGu
 {
+
+    public class UnityThreadBehaviour<T>
+        where T :class
+    {
+        public UnityThreadBehaviour(object sender, T action)
+        {
+            Sender = sender;
+            Action = action;
+        }
+
+        public object Sender { get; private set; }
+        public T Action { get; private set; }
+
+        public override string ToString()
+        {
+            return "[Sender:" + Sender.ToString() + ",Action:" + Action.ToString() + "]";
+        }
+    }
 
     /// <summary>
     /// 在Unity线程进行的操作;
@@ -22,10 +39,7 @@ namespace KouXiaGu
         ActionCollection onUpdate;
         ActionCollection onFixedUpdate;
 
-        LinkedListTracker<object> onUpdateTracker;
-        LinkedListTracker<object> onFixedUpdateTracker;
-
-        public IEnumerable<Action> UpdateObservers
+        public IEnumerable<UnityThreadBehaviour<Action>> UpdateObservers
         {
             get { return onUpdate.Observers; }
         }
@@ -35,7 +49,7 @@ namespace KouXiaGu
             get { return onUpdate.ObserverCount; }
         }
 
-        public IEnumerable<Action> FixedUpdateObservers
+        public IEnumerable<UnityThreadBehaviour<Action>> FixedUpdateObservers
         {
             get { return onFixedUpdate.Observers; }
         }
@@ -49,50 +63,70 @@ namespace KouXiaGu
         {
             onUpdate = new ActionCollection();
             onFixedUpdate = new ActionCollection();
-
-            onUpdateTracker = new LinkedListTracker<object>();
-            onFixedUpdateTracker = new LinkedListTracker<object>();
         }
 
         void Update()
         {
             onUpdate.Next();
-            //onUpdateTracker.Track(this);
         }
 
         void FixedUpdate()
         {
             onFixedUpdate.Next();
-            //onFixedUpdateTracker.Track(this);
         }
 
         /// <summary>
         /// 订阅到 Update 更新,每次更新都会调用 OnNext(),若返回异常,则输出异常到日志;
         /// </summary>
+        [Obsolete]
         public IDisposable SubscribeUpdate(IObserver<object> item)
         {
-            return onUpdate.Subscribe(() => item.OnNext(this));
-            //return onUpdateTracker.Subscribe(item);
+            return SubscribeUpdate(item, () => item.OnNext(this));
         }
 
         /// <summary>
         /// 订阅到 FixedUpdate 更新,只会更新观察者的 OnNext() 和 OnCompleted();
         /// </summary>
+        [Obsolete]
         public IDisposable SubscribeFixedUpdate(IObserver<object> item)
         {
-            return onFixedUpdate.Subscribe(() => item.OnNext(this));
-            //return onFixedUpdateTracker.Subscribe(item);
+            return SubscribeFixedUpdate(item, () => item.OnNext(this));
         }
+
+
+        public IDisposable SubscribeUpdate(object sender, Action action)
+        {
+            var behaviour = new UnityThreadBehaviour<Action>(sender, action);
+            return SubscribeUpdate(behaviour);
+        }
+
+        public IDisposable SubscribeUpdate(UnityThreadBehaviour<Action> behaviour)
+        {
+            return onUpdate.Subscribe(behaviour);
+        }
+
+
+        public IDisposable SubscribeFixedUpdate(object sender, Action action)
+        {
+            var behaviour = new UnityThreadBehaviour<Action>(sender, action);
+            return SubscribeFixedUpdate(behaviour);
+        }
+
+        public IDisposable SubscribeFixedUpdate(UnityThreadBehaviour<Action> behaviour)
+        {
+            return onFixedUpdate.Subscribe(behaviour);
+        }
+
 
         abstract class ActionCollectionBase<T> : ObserverCollection<T>
         {
             protected void OnError(T item, Exception ex)
             {
-                Debug.LogError(ex);
+                Debug.LogError("UnityThreadDispatcher:" + item.ToString() + ex);
             }
         }
 
-        class ActionCollection : ActionCollectionBase<Action>
+        class ActionCollection : ActionCollectionBase<UnityThreadBehaviour<Action>>
         {
             public void Next()
             {
@@ -100,7 +134,7 @@ namespace KouXiaGu
                 {
                     try
                     {
-                        item();
+                        item.Action();
                     }
                     catch (Exception ex)
                     {
