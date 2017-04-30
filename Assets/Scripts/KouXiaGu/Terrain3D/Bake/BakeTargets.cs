b11180d1b70e0e7b0dc2142bfc13cde73688ebf6
Jiong <Jiong@XIAGU>
2017-04-30 11:03:46 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/Terrain3D/Bake/BakeTargets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu.Terrain3D
+{
+
+    [Flags]
+    public enum BakeTargets
+    {
+        None = 0,
+        Landform = 1,
+        Road = 2,
+    }
+
+}
