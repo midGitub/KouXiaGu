f055f7a4b7ddd122b01f5dec8dedec2b70e10661
Jiong <Jiong@XIAGU>
2017-05-01 21:33:01 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/Terrain3D/Builder/WorldMapWatcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KouXiaGu.World.Map;
+
+namespace KouXiaGu.Terrain3D
+{
+
+    /// <summary>
+    /// 监视地图变化;
+    /// </summary>
+    public class WorldMapWatcher<T> : IDictionaryObserver<T, MapNode>
+    {
+        void IDictionaryObserver<T, MapNode>.OnAdded(T key, MapNode newValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDictionaryObserver<T, MapNode>.OnRemoved(T key, MapNode originalValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDictionaryObserver<T, MapNode>.OnUpdated(T key, MapNode originalValue, MapNode newValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
