e03874df91938b3f655ad224d83dc19653957703
Jiong <Jiong@XIAGU>
2017-05-05 18:18:42 +0800

    x

--- Assets/Scripts/KouXiaGu/Terrain3D/Chunk/LandformMesh.cs
+++ Assets/Scripts/KouXiaGu/Terrain3D/Chunk/LandformMesh.cs
@@ -18,8 +18,8 @@ namespace KouXiaGu.Terrain3D
 
         const string meshName = "Terrain Mesh";
 
-        static readonly float chunkHalfWidth = ChunkInfo.ChunkHalfWidth;
-        static readonly float chunkHalfHeight = ChunkInfo.ChunkHalfHeight;
+        static readonly float chunkHalfWidth = ChunkInfo.ChunkHalfWidth + 0.001f;
+        static readonly float chunkHalfHeight = ChunkInfo.ChunkHalfHeight + 0.001f;
 
         /// <summary>
         /// 网格生成的高度;
