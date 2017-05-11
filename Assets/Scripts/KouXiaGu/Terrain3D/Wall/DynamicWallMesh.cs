ba5c2cd1b21f9fad005243a02d7f88914000a654
Jiong <Jiong@XIAGU>
2017-05-11 13:36:56 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/Terrain3D/Wall/DynamicWallMesh.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KouXiaGu.Terrain3D
+{
+
+    /// <summary>
+    /// 动态墙体网格;
+    /// </summary>
+    [DisallowMultipleComponent]
+    class DynamicWallMesh : MonoBehaviour
+    {
+        DynamicWallMesh()
+        {
+        }
+
+
+
+
+    }
+
+}
