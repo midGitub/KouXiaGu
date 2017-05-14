bd22c9b0dc3056a11c090b268e3ab6f40fffcdff
Jiong <Jiong@XIAGU>
2017-05-15 00:44:27 +0800

    x

--- Assets/Scripts/KouXiaGu/Terrain3D/DynamicMesh/DynamicMeshManager.cs
+++ Assets/Scripts/KouXiaGu/Terrain3D/DynamicMesh/DynamicMeshManager.cs
@@ -14,12 +14,41 @@ namespace KouXiaGu.Terrain3D.Wall
         {
         }
 
+        [SerializeField]
+        List<SerializableDynamicMeshData> serializableData;
         Dictionary<string, DynamicMeshData> meshData;
+
+        public List<SerializableDynamicMeshData> SerializableData
+        {
+            get { return serializableData; }
+        }
 
         void Awake()
         {
             SetInstance(this);
         }
+
+        public void Add(string name, DynamicMeshData meshData)
+        {
+            
+        }
+
+    }
+
+    /// <summary>
+    /// 用于序列化保存;
+    /// </summary>
+    [Serializable]
+    public struct SerializableDynamicMeshData
+    {
+        public SerializableDynamicMeshData(string name, DynamicMeshData meshData)
+        {
+            Name = name;
+            MeshData = meshData;
+        }
+
+        public string Name;
+        public DynamicMeshData MeshData;
     }
 
 }
