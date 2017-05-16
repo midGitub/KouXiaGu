584da1f71e5f7223ce2489958f1ab3c872f448f6
Jiong <Jiong@XIAGU>
2017-05-16 17:49:20 +0800

    x

--- Assets/Scripts/KouXiaGu/Terrain3D/Builder/BuildingManager.cs
+++ Assets/Scripts/KouXiaGu/Terrain3D/Builder/BuildingManager.cs
@@ -7,116 +7,55 @@ using System.Text;
 
 namespace KouXiaGu.Terrain3D
 {
-
-
-    public interface IBuildingWatcher
+    public interface IBuildingWatcher : IChunkWatcher
     {
-        IEnumerable<RectCoord> GetDispaly();
     }
 
-    public class BuildingManager
+    public class BuildingManager : ChunkWatcherUpdater
     {
         static BuildingManager()
         {
             watcherList = new List<IBuildingWatcher>();
-            readOnlyWatcherList = watcherList.AsReadOnlyCollection();
         }
+
+        static readonly List<IBuildingWatcher> watcherList;
+
+        public static List<IBuildingWatcher> WatcherList
+        {
+            get { return watcherList; }
+        }
+
 
         public BuildingManager(IWorldData worldData, LandformManager landform)
         {
             builder = new BuildingBuilder(worldData, landform);
         }
 
-        static readonly List<IBuildingWatcher> watcherList;
-        static readonly IReadOnlyCollection<IBuildingWatcher> readOnlyWatcherList;
         readonly BuildingBuilder builder;
-        readonly HashSet<RectCoord> createCoords;
-        readonly List<RectCoord> destroyCoords;
-
-        public static IReadOnlyCollection<IBuildingWatcher> WatcherList
-        {
-            get { return readOnlyWatcherList; }
-        }
 
         public BuildingBuilder Builder
         {
             get { return builder; }
         }
 
-        IEnumerable<RectCoord> sceneCoords
+        protected override IEnumerable<IChunkWatcher> Watchers
+        {
+            get { return watcherList.Cast<IChunkWatcher>(); }
+        }
+
+        protected override IEnumerable<RectCoord> SceneCoords
         {
             get { return builder.SceneChunks.Keys; }
         }
 
-        void SendDisplay()
+        protected override void CreateAt(RectCoord coord)
         {
-            UpdateDispalyCoords();
-
-            IEnumerable<RectCoord> needDestroyCoords = GetNeedDestroyCoords();
-            foreach (var coord in needDestroyCoords)
-            {
-                this.builder.Destroy(coord);
-            }
-
-            IEnumerable<RectCoord> needCreateCoords = GetNeedCreateCoords();
-            foreach (var coord in needCreateCoords)
-            {
-                this.builder.Create(coord);
-            }
-
-            createCoords.Clear();
-            destroyCoords.Clear();
+            builder.Create(coord);
         }
 
-
-        IEnumerable<RectCoord> GetNeedDestroyCoords()
+        protected override void DestroyAt(RectCoord coord)
         {
-            foreach (var coord in sceneCoords)
-            {
-                if (!createCoords.Contains(coord))
-                    destroyCoords.Add(coord);
-            }
-            return destroyCoords;
-        }
-
-        IEnumerable<RectCoord> GetNeedCreateCoords()
-        {
-            return createCoords;
-        }
-
-        /// <summary>
-        /// 对场景创建管理进行更新;
-        /// </summary>
-        class BuildRequestUpdater : IUnityThreadBehaviour<Action>
-        {
-            public BuildRequestUpdater(BuildingManager manager)
-            {
-                Manager = manager;
-                this.SubscribeUpdate();
-            }
-
-            readonly BuildingManager Manager;
-
-            object IUnityThreadBehaviour<Action>.Sender
-            {
-                get { return "场景的地形块创建销毁管理"; }
-            }
-
-            Action IUnityThreadBehaviour<Action>.Action
-            {
-                get { return Manager.SendDisplay; }
-            }
-
-        }
-
-        void UpdateDispalyCoords()
-        {
-            foreach (var watcher in watcherList)
-            {
-                var displayChunkCoords = watcher.GetDispaly();
-                createCoords.IntersectWith(displayChunkCoords);
-            }
+            builder.Destroy(coord);
         }
     }
-
 }
