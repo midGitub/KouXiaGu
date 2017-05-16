3af9943e01839dd15558d707423f8466475152dd
Jiong <Jiong@XIAGU>
2017-05-16 09:48:29 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/Terrain3D/Builder/BuildingBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KouXiaGu.World;
+using KouXiaGu.Grids;
+
+namespace KouXiaGu.Terrain3D
+{
+
+    public interface ILandformBuilding
+    {
+        void Build(CubicHexCoord coord, Landform landform, IWorldData data);
+        void Destroy();
+    }
+
+    public class BuildingBuilder
+    {
+        public BuildingBuilder()
+        {
+        }
+
+
+
+    }
+
+}
