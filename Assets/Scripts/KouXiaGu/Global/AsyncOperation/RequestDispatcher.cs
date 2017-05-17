a56672e633f900283f3e5e500d551dad427a21fd
Jiong <Jiong@XIAGU>
2017-05-17 12:09:17 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/Global/AsyncOperation/RequestDispatcher.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KouXiaGu
+{
+
+    public interface IRequest
+    {
+        bool IsCanceled { get; }
+        void Operate();
+        void AddQueue();
+        void OutQueue();
+    }
+
+    /// <summary>
+    /// 异步请求处理;
+    /// </summary>
+    public sealed class RequestDispatcher : MonoBehaviour
+    {
+        RequestDispatcher()
+        {
+        }
+
+        [SerializeField]
+        Stopwatch runtimeStopwatch;
+        Queue<IRequest> requestQueue;
+        Coroutine createrCoroutine;
+
+        void Awake()
+        {
+            requestQueue = new Queue<IRequest>();
+            createrCoroutine = new Coroutine(Coroutine());
+        }
+
+        void Update()
+        {
+            createrCoroutine.MoveNext();
+        }
+
+        public void AddQueue(IRequest request)
+        {
+            request.AddQueue();
+            requestQueue.Enqueue(request);
+        }
+
+        IEnumerator Coroutine()
+        {
+            while (true)
+            {
+                while (requestQueue.Count == 0)
+                {
+                    yield return null;
+                }
+
+                try
+                {
+                    IRequest request = requestQueue.Dequeue();
+
+                    if (!request.IsCanceled)
+                    {
+                        request.Operate();
+                    }
+
+                    request.OutQueue();
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError(ex);
+                }
+
+                if (runtimeStopwatch.Await())
+                {
+                    yield return null;
+                    runtimeStopwatch.Restart();
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/KouXiaGu/Terrain3D/_Old/Creater/LandformBake/IRequest.cs
+++ Assets/Scripts/KouXiaGu/Terrain3D/_Old/Creater/LandformBake/IRequest.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using KouXiaGu.Grids;
-using KouXiaGu.World.Map;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using KouXiaGu.Grids;
+//using KouXiaGu.World.Map;
 
-namespace KouXiaGu.Terrain3D
-{
+//namespace KouXiaGu.Terrain3D
+//{
 
 
-    public interface IRequest
-    {
+//    public interface IRequest
+//    {
 
-        /// <summary>
-        /// 地图块的坐标
-        /// </summary>
-        RectCoord ChunkCoord { get; }
+//        /// <summary>
+//        /// 地图块的坐标
+//        /// </summary>
+//        RectCoord ChunkCoord { get; }
 
-        /// <summary>
-        /// 地形使用的地图;
-        /// </summary>
-        IDictionary<CubicHexCoord, MapNode> Data { get; }
+//        /// <summary>
+//        /// 地形使用的地图;
+//        /// </summary>
+//        IDictionary<CubicHexCoord, MapNode> Data { get; }
 
-    }
+//    }
 
-}
+//}
