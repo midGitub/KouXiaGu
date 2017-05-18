da11615d66c3459bfb256cb99b37f1c1862ddd17
Jiong <Jiong@XIAGU>
2017-05-18 21:04:14 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/World/Map/New/MapDataReader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu.World.Map
+{
+
+    public interface IMapDataReader
+    {
+        /// <summary>
+        /// 异步读取到地图数据;
+        /// </summary>
+        IAsyncOperation<MapData> ReadAsync(IGameData info);
+    }
+
+    abstract class MapDataReader
+    {
+
+    }
+
+    class RandomMapDataCreater : MapDataReader
+    {
+
+    }
+
+}
