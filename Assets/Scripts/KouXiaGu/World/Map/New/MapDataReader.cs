68d6d81d436f96faedde6cdc1a7151905992dc6a
Jiong <Jiong@XIAGU>
2017-05-18 21:11:34 +0800

    x

--- Assets/Scripts/KouXiaGu/World/Map/New/MapDataReader.cs
+++ Assets/Scripts/KouXiaGu/World/Map/New/MapDataReader.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KouXiaGu.Grids;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +15,65 @@ namespace KouXiaGu.World.Map
         IAsyncOperation<MapData> ReadAsync(IGameData info);
     }
 
-    abstract class MapDataReader
+    class RandomMapDataCreater : IMapDataReader
     {
+        public RandomMapDataCreater(int mapSize)
+        {
+            MapSize = mapSize;
+        }
 
+        public int MapSize { get; set; }
+
+        public MapData Read(IGameData info)
+        {
+            int[] landformArray = info.Terrain.LandformInfos.Keys.ToArray();
+            int[] buildArray = info.Terrain.BuildingInfos.Keys.ToArray();
+            Dictionary<CubicHexCoord, MapNode> map = new Dictionary<CubicHexCoord, MapNode>();
+            var points = CubicHexCoord.Range(CubicHexCoord.Self, MapSize);
+
+            foreach (var point in points)
+            {
+                MapNode node = new MapNode()
+                {
+                    Landform = new LandformNode()
+                    {
+                        Type = Random(landformArray),
+                        Angle = RandomAngle(),
+                    },
+
+                    Building = new BuildingNode()
+                    {
+                        Type = Random(buildArray),
+                        Angle = RandomAngle(),
+                    },
+                };
+                map.Add(point, node);
+            }
+
+            MapData data = new MapData()
+            {
+                Map = map,
+            };
+
+            return data;
+        }
+
+        public IAsyncOperation<MapData> ReadAsync(IGameData info)
+        {
+            return new ThreadDelegateOperation<MapData>(() => Read(info));
+        }
+
+        static readonly System.Random random = new System.Random();
+
+        T Random<T>(T[] array)
+        {
+            int index = random.Next(0, array.Length);
+            return array[index];
+        }
+
+        int RandomAngle()
+        {
+            return random.Next(0, 360);
+        }
     }
-
-    class RandomMapDataCreater : MapDataReader
-    {
-
-    }
-
 }
