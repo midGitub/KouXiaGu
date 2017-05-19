25be3bc61cf2903962332bbe2fb01a7748ca59da
Jiong <Jiong@XIAGU>
2017-05-19 11:43:57 +0800

    x

--- Assets/Scripts/KouXiaGu/World/Map/New/DataArchive.cs
+++ Assets/Scripts/KouXiaGu/World/Map/New/DataArchive.cs
@@ -1,168 +1,79 @@
-﻿using KouXiaGu.Grids;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Collections;
+﻿//using KouXiaGu.Grids;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Collections;
 
-namespace KouXiaGu.World.Map
-{
+//namespace KouXiaGu.World.Map
+//{
 
-    /// <summary>
-    /// 记录地图变化;
-    /// </summary>
-    public class DataArchive : IDictionary<CubicHexCoord, MapNode>, IReadOnlyDictionary<CubicHexCoord, MapNode>
-    {
-        public DataArchive(MapData data)
-        {
-            this.data = data;
-            changedPositions = new HashSet<CubicHexCoord>();
-        }
+//    /// <summary>
+//    /// 记录地图变化;
+//    /// </summary>
+//    class DataArchive : IDictionaryObserver<CubicHexCoord, MapNode>
+//    {
+//        public DataArchive(MapData data, IObservableDictionary<CubicHexCoord, MapNode> observableMap)
+//        {
+//            this.data = data;
+//            this.observableMap = observableMap;
+//            changedPositions = new HashSet<CubicHexCoord>();
+//            observableMap.Subscribe(this);
+//        }
 
-        readonly MapData data;
-        readonly HashSet<CubicHexCoord> changedPositions;
-        readonly IObserverCollection<IDictionaryObserver<CubicHexCoord, MapNode>> observers;
+//        readonly MapData data;
+//        readonly IObservableDictionary<CubicHexCoord, MapNode> observableMap;
+//        readonly HashSet<CubicHexCoord> changedPositions;
 
-        IDictionary<CubicHexCoord, MapNode> map
-        {
-            get { return data.Map; }
-        }
+//        IDictionary<CubicHexCoord, MapNode> map
+//        {
+//            get { return data.Map; }
+//        }
 
-        public MapData Data
-        {
-            get { return data; }
-        }
+//        public ICollection<CubicHexCoord> ChangedPositions
+//        {
+//            get { return changedPositions; }
+//        }
 
-        /// <summary>
-        /// 发生变化的节点,不包括被移除的节点;
-        /// </summary>
-        public ICollection<CubicHexCoord> ChangedPositions
-        {
-            get { return changedPositions; }
-        }
+//        void IDictionaryObserver<CubicHexCoord, MapNode>.OnAdded(CubicHexCoord key, MapNode newValue)
+//        {
+//            changedPositions.Add(key);
+//        }
 
-        public ICollection<CubicHexCoord> Keys
-        {
-            get { return map.Keys; }
-        }
+//        void IDictionaryObserver<CubicHexCoord, MapNode>.OnRemoved(CubicHexCoord key, MapNode originalValue)
+//        {
+//            changedPositions.Remove(key);
+//        }
 
-        public ICollection<MapNode> Values
-        {
-            get { return map.Values; }
-        }
+//        void IDictionaryObserver<CubicHexCoord, MapNode>.OnUpdated(CubicHexCoord key, MapNode originalValue, MapNode newValue)
+//        {
+//            return;
+//        }
 
-        IEnumerable<CubicHexCoord> IReadOnlyDictionary<CubicHexCoord, MapNode>.Keys
-        {
-            get { return map.Keys; }
-        }
+//        /// <summary>
+//        /// 获取变化的到用于归档的数据;
+//        /// </summary>
+//        public MapData GetArchivedData()
+//        {
+//            MapData archivedData = new MapData()
+//            {
+//                Map = GetChangedData(),
+//            };
+//            return archivedData;
+//        }
 
-        IEnumerable<MapNode> IReadOnlyDictionary<CubicHexCoord, MapNode>.Values
-        {
-            get { return map.Values; }
-        }
-
-        public int Count
-        {
-            get { return map.Count; }
-        }
-
-        public bool IsReadOnly
-        {
-            get { return false; }
-        }
-
-        public MapNode this[CubicHexCoord key]
-        {
-            get { return map[key]; }
-            set
-            {
-                map[key] = value;
-                changedPositions.Add(key);
-            }
-        }
-
-        public void Add(KeyValuePair<CubicHexCoord, MapNode> item)
-        {
-            Add(item.Key, item.Value);
-        }
-
-        public void Add(CubicHexCoord key, MapNode value)
-        {
-            map.Add(key, value);
-            changedPositions.Add(key);
-        }
-
-        public bool Remove(KeyValuePair<CubicHexCoord, MapNode> item)
-        {
-            return Remove(item.Key);
-        }
-
-        public bool Remove(CubicHexCoord key)
-        {
-            bool isRemoved = map.Remove(key);
-            changedPositions.Remove(key);
-            return isRemoved;
-        }
-
-        public bool ContainsKey(CubicHexCoord key)
-        {
-            return map.ContainsKey(key);
-        }
-
-        public bool Contains(KeyValuePair<CubicHexCoord, MapNode> item)
-        {
-            return map.ContainsKey(item.Key);
-        }
-
-        public bool TryGetValue(CubicHexCoord key, out MapNode value)
-        {
-            return map.TryGetValue(key, out value);
-        }
-
-        public void Clear()
-        {
-            map.Clear();
-        }
-
-        void ICollection<KeyValuePair<CubicHexCoord, MapNode>>.CopyTo(KeyValuePair<CubicHexCoord, MapNode>[] array, int arrayIndex)
-        {
-            (map as ICollection<KeyValuePair<CubicHexCoord, MapNode>>).CopyTo(array, arrayIndex);
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public IEnumerator<KeyValuePair<CubicHexCoord, MapNode>> GetEnumerator()
-        {
-            return map.GetEnumerator();
-        }
-
-        /// <summary>
-        /// 获取变化的到用于归档的数据;
-        /// </summary>
-        public MapData GetArchivedData()
-        {
-            MapData archivedData = new MapData()
-            {
-                Map = GetChangedData(),
-            };
-            return archivedData;
-        }
-
-        /// <summary>
-        /// 获取到发生变化的节点结构;
-        /// </summary>
-        public Dictionary<CubicHexCoord, MapNode> GetChangedData()
-        {
-            Dictionary<CubicHexCoord, MapNode> changedData = new Dictionary<CubicHexCoord, MapNode>();
-            foreach (var position in changedPositions)
-            {
-                MapNode node = map[position];
-                changedData.Add(position, node);
-            }
-            return changedData;
-        }
-    }
-}
+//        /// <summary>
+//        /// 获取到发生变化的节点结构;
+//        /// </summary>
+//        public Dictionary<CubicHexCoord, MapNode> GetChangedData()
+//        {
+//            Dictionary<CubicHexCoord, MapNode> changedData = new Dictionary<CubicHexCoord, MapNode>();
+//            foreach (var position in changedPositions)
+//            {
+//                MapNode node = map[position];
+//                changedData.Add(position, node);
+//            }
+//            return changedData;
+//        }
+//    }
+//}
--- Assets/Scripts/KouXiaGu/World/Map/New/GameMap.cs
+++ Assets/Scripts/KouXiaGu/World/Map/New/GameMap.cs
@@ -12,19 +12,20 @@ namespace KouXiaGu.World.Map
     /// </summary>
     public class GameMap
     {
-        public GameMap()
+        public GameMap() : this(new MapData())
         {
-            data = new MapData();
         }
 
         public GameMap(MapData data)
         {
             this.data = data;
             observableMap = new ObservableDictionary<CubicHexCoord, MapNode>(data.Map);
+            archive = new DataArchive(data, observableMap);
         }
 
         readonly MapData data;
         readonly ObservableDictionary<CubicHexCoord, MapNode> observableMap;
+        readonly DataArchive archive;
 
         internal MapData Data
         {
@@ -47,10 +48,73 @@ namespace KouXiaGu.World.Map
         }
 
 
-        class DataArchive
+        /// <summary>
+        /// 记录地图变化;
+        /// </summary>
+        class DataArchive : IDictionaryObserver<CubicHexCoord, MapNode>
         {
+            public DataArchive(MapData data, IObservableDictionary<CubicHexCoord, MapNode> observableMap)
+            {
+                this.data = data;
+                this.observableMap = observableMap;
+                changedPositions = new HashSet<CubicHexCoord>();
+                observableMap.Subscribe(this);
+            }
 
+            readonly MapData data;
+            readonly IObservableDictionary<CubicHexCoord, MapNode> observableMap;
+            readonly HashSet<CubicHexCoord> changedPositions;
+
+            IDictionary<CubicHexCoord, MapNode> map
+            {
+                get { return data.Map; }
+            }
+
+            public ICollection<CubicHexCoord> ChangedPositions
+            {
+                get { return changedPositions; }
+            }
+
+            void IDictionaryObserver<CubicHexCoord, MapNode>.OnAdded(CubicHexCoord key, MapNode newValue)
+            {
+                changedPositions.Add(key);
+            }
+
+            void IDictionaryObserver<CubicHexCoord, MapNode>.OnRemoved(CubicHexCoord key, MapNode originalValue)
+            {
+                changedPositions.Remove(key);
+            }
+
+            void IDictionaryObserver<CubicHexCoord, MapNode>.OnUpdated(CubicHexCoord key, MapNode originalValue, MapNode newValue)
+            {
+                return;
+            }
+
+            /// <summary>
+            /// 获取变化的到用于归档的数据;
+            /// </summary>
+            public MapData GetArchivedData()
+            {
+                MapData archivedData = new MapData()
+                {
+                    Map = GetChangedData(),
+                };
+                return archivedData;
+            }
+
+            /// <summary>
+            /// 获取到发生变化的节点结构;
+            /// </summary>
+            public Dictionary<CubicHexCoord, MapNode> GetChangedData()
+            {
+                Dictionary<CubicHexCoord, MapNode> changedData = new Dictionary<CubicHexCoord, MapNode>();
+                foreach (var position in changedPositions)
+                {
+                    MapNode node = map[position];
+                    changedData.Add(position, node);
+                }
+                return changedData;
+            }
         }
     }
-
 }
