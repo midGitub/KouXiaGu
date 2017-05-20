bbb11c51f50bdc7dd4b31030d4e44e82dda474d3
Jiong <Jiong@XIAGU>
2017-05-20 22:21:55 +0800

    x

---
+++ Assets/Scripts/KouXiaGu/Global/Extensions/CollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu.Collections
+{
+
+
+    public static class CollectionExtensions
+    {
+
+        /// <summary>
+        /// 移除元素;
+        /// </summary>
+        /// <param name="item">要在序列中定位的值</param>
+        /// <param name="comparer">一个对值进行比较的相等比较器;</param>
+        public static bool Remove<T>(IList<T> collection, T item, IEqualityComparer<T> comparer)
+        {
+            int index = FindIndex(collection, item, comparer);
+            if (index >= 0)
+            {
+                collection.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 获取到对应元素下标,若不存在则返回-1;
+        /// </summary>
+        /// <param name="item">要在序列中定位的值</param>
+        /// <param name="comparer">一个对值进行比较的相等比较器;</param>
+        public static int FindIndex<T>(IList<T> collection, T item, IEqualityComparer<T> comparer)
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                T original = collection[i];
+                if (comparer.Equals(original, item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/KouXiaGu/Global/Rx/Set.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu
+{
+
+
+    public class Set<T> : ICollection<T>
+    {
+        public Set()
+        {
+            collection = new List<T>();
+            comparer = EqualityComparer<T>.Default;
+        }
+
+        public Set(IEqualityComparer<T> comparer)
+        {
+            collection = new List<T>();
+            this.comparer = comparer;
+        }
+
+        readonly List<T> collection;
+        IEqualityComparer<T> comparer;
+
+        public IEqualityComparer<T> Comparer
+        {
+            get { return comparer; }
+        }
+
+        public int Count
+        {
+            get { return collection.Count; }
+        }
+
+        bool ICollection<T>.IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public void Add(T item)
+        {
+            if (collection.Contains(item, comparer))
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                collection.Add(item);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            return collection.Remove(item);
+        }
+
+        public void Clear()
+        {
+            collection.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return collection.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            collection.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((ICollection<T>)collection).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((ICollection<T>)collection).GetEnumerator();
+        }
+    }
+
+}
