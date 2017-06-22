97abaa0690fc12f277c07fd927ca9a8f80254bb3
Jiong <Jiong@XIAGU>
2017-06-23 02:48:16 +0800

    操作记录

---
+++ Assets/Scripts/KouXiaGu/Global/Collections/LinkedList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu.Collections
+{
+
+    /// <summary>
+    /// 自定义实现的双向链接(非循环);
+    /// </summary>
+    public class cLinkedList<T>
+    {
+
+        public int Count { get; private set; }
+        public LinkedListNode<T> First { get; private set; }
+        public LinkedListNode<T> Last { get; private set; }
+
+        public LinkedListNode<T> AddAfter(LinkedListNode<T> node, T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddAfter(LinkedListNode<T> node, LinkedListNode<T> newNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        void InsertNodeBefore_internal(LinkedListNode<T> node, LinkedListNode<T> newNode)
+        {
+            newNode.Next = node;
+            newNode.Previous = node.Previous;
+            node.Previous.Next = newNode;
+            node.Previous = newNode;
+            Count++;
+        }
+
+        void InsertNodeToEmptyList_internal(LinkedListNode<T> newNode)
+        {
+            First = newNode;
+            Last = newNode;
+            Count++;
+        }
+
+        void RemoveNode_internal(LinkedListNode<T> node)
+        {
+            if (node.Next == null)
+            {
+                Last = node.Previous;
+            }
+            else
+            {
+                node.Next.Previous = node.Previous;
+            }
+
+            if (node.Previous == null)
+            {
+                First = node.Next;
+            }
+            else
+            {
+                node.Previous.Next = node.Next;
+            }
+
+            node.Invalidate();
+            Count--;
+        }
+
+        public void Remove(LinkedListNode<T> node)
+        {
+            ValidateNode(node);
+            RemoveNode_internal(node);
+        }
+
+        void ValidateNode(LinkedListNode<T> node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException("node");
+            }
+
+            if (node.List != this)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+
+    public sealed class LinkedListNode<T>
+    {
+        public LinkedListNode(cLinkedList<T> list, T value)
+        {
+            List = list;
+            Value = value;
+        }
+
+        public cLinkedList<T> List { get; internal set; }
+        public LinkedListNode<T> Next { get; internal set; }
+        public LinkedListNode<T> Previous { get; internal set; }
+        public T Value { get; internal set; }
+
+        internal void Invalidate()
+        {
+            List = null;
+            Next = null;
+            Previous = null;
+        }
+    }
+}
---
+++ Assets/Scripts/KouXiaGu/Global/OperationRecord/OperationRecorder.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KouXiaGu.OperationRecord
+{
+
+    /// <summary>
+    /// 可撤销的操作;
+    /// </summary>
+    public interface IVoidableOperation
+    {
+        /// <summary>
+        /// 重新执行命令;
+        /// </summary>
+        void Redo();
+
+        /// <summary>
+        /// 撤销这操作;
+        /// </summary>
+        void Undo();
+    }
+
+    /// <summary>
+    /// 操作记录;
+    /// </summary>
+    public class OperationRecorder<T>
+        where T : IVoidableOperation
+    {
+        public OperationRecorder(int maxRecord)
+        {
+            if (maxRecord <= 0)
+                throw new ArgumentOutOfRangeException("maxRecord :" + maxRecord);
+
+            operationQueue = new T[maxRecord];
+            currentIndex = 0;
+            lastIndex = 0;
+            Count = 0;
+        }
+
+        readonly T[] operationQueue;
+
+        /// <summary>
+        /// 指向第一条指令位置;
+        /// </summary>
+        int currentIndex;
+
+        /// <summary>
+        /// 指向最后一条指令位置;
+        /// </summary>
+        int lastIndex;
+
+        /// <summary>
+        /// 指令总数;
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// 最大记录;
+        /// </summary>
+        public int MaxRecord
+        {
+            get { return operationQueue.Length; }
+        }
+
+        public bool IsFull
+        {
+            get { return Count == MaxRecord; }
+        }
+
+        /// <summary>
+        /// 记录这个操作;
+        /// </summary>
+        public void Register(T operation)
+        {
+            if (operation == null)
+                throw new ArgumentNullException("operation");
+
+            operationQueue[lastIndex] = operation;
+            lastIndex = MoveNext(lastIndex);
+            if (IsFull)
+            {
+                currentIndex = MoveNext(currentIndex);
+            }
+            else
+            {
+                Count++;
+            }
+        }
+
+        /// <summary>
+        /// 将下标向后移动一个单位;
+        /// </summary>
+        int MoveNext(int index)
+        {
+            index++;
+            if (index >= operationQueue.Length)
+            {
+                index = 0;
+            }
+            return index;
+        }
+
+        /// <summary>
+        /// 将下标向前移动一个单位;
+        /// </summary>
+        int MovePrevious(int index)
+        {
+            index--;
+            if (index < 0)
+            {
+                index = operationQueue.Length - 1;
+            }
+            return index;
+        }
+
+        /// <summary>
+        /// 执行撤销;
+        /// </summary>
+        public void PerformUndo()
+        {
+
+
+            var operation = operationQueue[lastIndex];
+            if (operation == null)
+            {
+                return;
+            }
+            operation.Undo();
+            lastIndex = MovePrevious(lastIndex);
+        }
+
+        /// <summary>
+        /// 执行重做;
+        /// </summary>
+        public void PerformRedo()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 清除所有记录;
+        /// </summary>
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
