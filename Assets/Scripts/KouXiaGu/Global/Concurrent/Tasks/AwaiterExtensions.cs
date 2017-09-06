f6c0e0c567f33ebb1a34bc6238b5e01639cd9b1a
Jiong <Jiong@XIAGU>
2017-09-06 20:17:46 +0800

    x

--- Assets/Scripts/KouXiaGu/Global/Concurrent/Tasks/AwaiterExtensions.cs
+++ Assets/Scripts/KouXiaGu/Global/Concurrent/Tasks/AwaiterExtensions.cs
@@ -1,132 +1,131 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Text;
+//using System.Threading.Tasks;
+//using UnityEngine;
 
-namespace KouXiaGu.Concurrent
-{
-
-
-    public static class AwaiterExtensions
-    {
+//namespace KouXiaGu.Concurrent
+//{
 
 
-        public static RequestAwaiter GetAwaiter(this IRequest request)
-        {
-            return new RequestAwaiter(request);
-        }
+//    public static class AwaiterExtensions
+//    {
 
-        public struct RequestAwaiter : INotifyCompletion
-        {
-            public RequestAwaiter(IRequest request)
-            {
-                this.request = request;
-            }
+//        public static RequestAwaiter GetAwaiter(this IRequest request)
+//        {
+//            return new RequestAwaiter(request);
+//        }
 
-            IRequest request;
+//        public struct RequestAwaiter : INotifyCompletion
+//        {
+//            public RequestAwaiter(IRequest request)
+//            {
+//                this.request = request;
+//            }
 
-            public bool IsCompleted
-            {
-                get { return request.IsCompleted; }
-            }
+//            IRequest request;
 
-            public void OnCompleted(Action continuation)
-            {
-                RequestAwaiter t = this;
-                Task.Run(delegate ()
-                {
-                    while (!t.request.IsCompleted)
-                    {
-                    }
-                    continuation();
-                });
-            }
+//            public bool IsCompleted
+//            {
+//                get { return request.IsCompleted; }
+//            }
 
-            public void GetResult()
-            {
-                return;
-            }
-        }
+//            public void OnCompleted(Action continuation)
+//            {
+//                RequestAwaiter t = this;
+//                Task.Run(delegate ()
+//                {
+//                    while (!t.request.IsCompleted)
+//                    {
+//                    }
+//                    continuation();
+//                });
+//            }
 
-
-
-        public static RequestAwaiter<T> GetAwaiter<T>(this IRequest<T> request)
-        {
-            return new RequestAwaiter<T>(request);
-        }
-
-        public struct RequestAwaiter<T> : INotifyCompletion
-        {
-            public RequestAwaiter(IRequest<T> request)
-            {
-                this.request = request;
-            }
-
-            IRequest<T> request;
-
-            public bool IsCompleted
-            {
-                get { return request.IsCompleted; }
-            }
-
-            public void OnCompleted(Action continuation)
-            {
-                RequestAwaiter<T> t = this;
-                Task.Run(delegate()
-                {
-                    while (!t.request.IsCompleted)
-                    {
-                    }
-                    continuation();
-                });
-            }
-
-            void OnCompletedInternal(Action continuation)
-            {
-                while (!request.IsCompleted)
-                {
-                }
-                continuation();
-            }
-
-            public T GetResult()
-            {
-                return request.Result;
-            }
-        }
+//            public void GetResult()
+//            {
+//                return;
+//            }
+//        }
 
 
-        public static AssetBundleCreateRequestAwaiter GetAwaiter(this AssetBundleCreateRequest request)
-        {
-            return new AssetBundleCreateRequestAwaiter(request);
-        }
 
-        public struct AssetBundleCreateRequestAwaiter : INotifyCompletion
-        {
-            public AssetBundleCreateRequestAwaiter(AssetBundleCreateRequest request)
-            {
-                this.request = request;
-            }
+//        public static RequestAwaiter<T> GetAwaiter<T>(this IRequest<T> request)
+//        {
+//            return new RequestAwaiter<T>(request);
+//        }
 
-            AssetBundleCreateRequest request;
+//        public struct RequestAwaiter<T> : INotifyCompletion
+//        {
+//            public RequestAwaiter(IRequest<T> request)
+//            {
+//                this.request = request;
+//            }
 
-            public bool IsCompleted
-            {
-                get { return request.isDone; }
-            }
+//            IRequest<T> request;
 
-            public void OnCompleted(Action continuation)
-            {
-                continuation();
-            }
+//            public bool IsCompleted
+//            {
+//                get { return request.IsCompleted; }
+//            }
 
-            public AssetBundle GetResult()
-            {
-                return request.assetBundle;
-            }
-        }
-    }
-}
+//            public void OnCompleted(Action continuation)
+//            {
+//                RequestAwaiter<T> t = this;
+//                Task.Run(delegate()
+//                {
+//                    while (!t.request.IsCompleted)
+//                    {
+//                    }
+//                    continuation();
+//                });
+//            }
+
+//            void OnCompletedInternal(Action continuation)
+//            {
+//                while (!request.IsCompleted)
+//                {
+//                }
+//                continuation();
+//            }
+
+//            public T GetResult()
+//            {
+//                return request.Result;
+//            }
+//        }
+
+
+//        public static AssetBundleCreateRequestAwaiter GetAwaiter(this AssetBundleCreateRequest request)
+//        {
+//            return new AssetBundleCreateRequestAwaiter(request);
+//        }
+
+//        public struct AssetBundleCreateRequestAwaiter : INotifyCompletion
+//        {
+//            public AssetBundleCreateRequestAwaiter(AssetBundleCreateRequest request)
+//            {
+//                this.request = request;
+//            }
+
+//            AssetBundleCreateRequest request;
+
+//            public bool IsCompleted
+//            {
+//                get { return request.isDone; }
+//            }
+
+//            public void OnCompleted(Action continuation)
+//            {
+//                continuation();
+//            }
+
+//            public AssetBundle GetResult()
+//            {
+//                return request.assetBundle;
+//            }
+//        }
+//    }
+//}
